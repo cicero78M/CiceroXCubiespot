98b5be23e421dc15f8dd9c3996eb2cee675d1dc8
papiqo <nationalite45@gmail.com>
2024-12-17 19:48:41 +0700

    UPdate Unit Test

---
+++ unitTest/collecting/insta/instaAPI/instaLikesAPI.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+
+const fs = require('fs');
+const ciceroKey = JSON.parse (fs.readFileSync('ciceroKey.json'));
+
+const headers = {
+  'x-cache-control': 'no-cache',
+  'x-rapidapi-key': ciceroKey.instaKey.instakeyAPI,
+  'x-rapidapi-host': ciceroKey.instaKey.instahostAPI
+}
+
+
+module.exports = {  
+
+    instaLikesAPI: async function instaLikesAPI(key){
+        //Insta Likes API
+        let options = {
+            method: 'GET',
+            url: googleCreds.instaKey.instahostLikes,
+            params: {
+            code_or_id_or_url: key
+            },
+            headers: headers
+        };
+
+        try {
+            let response = await axios.request(options);
+
+            let data = {
+                data : response.data,
+                code : 200,
+                state : true
+            }
+            
+            return data;        
+        
+        } catch (error) {
+
+            let data = {
+                data : error,
+                code : 303,
+                state : false
+            }
+            
+           return data;         
+        }
+    }
+
+}
