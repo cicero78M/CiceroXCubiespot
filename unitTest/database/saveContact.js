fc24c2be74dea484a23366124e1a4840b2f0d61d
papiqo <nationalite45@gmail.com>
2024-12-25 17:54:30 +0700

    Update

--- unitTest/database/saveContact.js
+++ unitTest/database/saveContact.js
@@ -15,11 +15,25 @@ const googleAuth = new JWT({
 
 
 export async function saveContacts() {
+
+    try {
     const waContactDoc = new GoogleSpreadsheet(ciceroKey.dbKey.waContact, googleAuth); //Google Auth
     await waContactDoc.loadInfo(); // loads document properties and worksheets
 
     let whatsappList = ['',];
     let contactData = [];
+
+    let contactResponse = await sheetDoc(ciceroKey.dbKey.clientDataID, 'CONTACT');
+    let contactRows = contactResponse.data;
+    let contactList = [];
+
+    for (let i = 0; i < contactRows.length; i++){
+        if(!contactList.includes(contactRows[ii].get('MOBILE PHONE'))){ 
+            contactList.push(contactRows[ii].get('MOBILE PHONE'));
+
+        }
+    }
+
 
     let clientResponse = await sheetDoc(ciceroKey.dbKey.clientDataID, 'ClientData');
     let clientRows = clientResponse.data;
@@ -33,26 +47,25 @@ export async function saveContacts() {
                 for (let ii = 0; ii < userRows.length; ii++){
                     if(userRows[ii].get('WHATSAPP') != clientRows[i].get('OPERATOR') || userRows[ii].get('WHATSAPP') != null|| userRows[ii].get('WHATSAPP') != undefined || userRows[ii].get('WHATSAPP') != "" ){
                         
-                        if(!whatsappList.includes(userRows[ii].get('WHATSAPP'))){ 
+                        if(!contactList.includes(userRows[ii].get('WHATSAPP')) || !whatsappList.includes(userRows[ii].get('WHATSAPP'))){ 
                             whatsappList.push(userRows[ii].get('WHATSAPP'));
                             console.log('+'+userRows[ii].get('WHATSAPP'));
-                            contactData.push({'FIRST NAME': userRows[ii].get('NAMA'), 'LAST NAME' : '',EMAIL : '', 'MOBILE PHONE': '+'+userRows[ii].get('WHATSAPP'), COMPANY:  clientRows[i].get('CLIENT_ID')});
+                            contactData.push({'FIRST NAME': userRows[ii].get('NAMA'), 'LAST NAME' : '',EMAIL : '', 'MOBILE PHONE': +userRows[ii].get('WHATSAPP'), COMPANY:  clientRows[i].get('CLIENT_ID')});
                     
                         }
                     }
                 }
             }
         }
-        try {
             const waSheet = waContactDoc.sheetsByTitle['CONTACT'];
             waSheet.addRows(contactData);
     
             return 'SUCCESS!!!!';
-        } catch (error) {
-            console.log(error);
-        }
 
-    }    
+        }    
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
