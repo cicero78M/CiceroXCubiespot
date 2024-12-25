b9cafa4846e690d1e3da8ca6f63e3c6258139b63
papiqo <nationalite45@gmail.com>
2024-12-25 12:19:39 +0700

    Update

--- unitTest/database/saveContact.js
+++ unitTest/database/saveContact.js
@@ -30,7 +30,7 @@ export async function saveContacts() {
                 let userResponse = await sheetDoc(ciceroKey.dbKey.userDataID, clientRows[i].get('CLIENT_ID'));
                 let userRows = userResponse.data;
 
-                for (let ii = 0; ii < userRows.length; ii++){
+                for (let ii = 1; ii < userRows.length; ii++){
                     if(userRows[ii].get('WHATSAPP') != clientRows[i].get('OPERATOR') || userRows[ii].get('WHATSAPP') != null|| userRows[ii].get('WHATSAPP') != undefined || userRows[ii].get('WHATSAPP') != "" ){
                         
                         if(!whatsappList.includes(userRows[ii].get('WHATSAPP'))){ 
