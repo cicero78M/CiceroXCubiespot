adc0c5769fedaddf297ed301310b3ab4a21c4b5c
papiqo <nationalite45@gmail.com>
2024-12-25 07:56:18 +0700

    Update

--- app.js
+++ app.js
@@ -279,7 +279,7 @@ client.on('message', async (msg) => {
                 let body = msg.body;
                 let url = body.match(/\bhttps?:\/\/\S+/gi);
 
-                let splittedUrl = url.replaceAll("?", "");
+                let splittedUrl = url.replace("?", "");
 
                 if (splittedUrl.length >=1){
                if (splittedUrl.includes("www.instagram.com")){
