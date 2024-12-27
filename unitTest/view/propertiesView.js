747048e1f6dec0f8675491258889eaebc93a7d9b
papiqo <nationalite45@gmail.com>
2024-12-27 07:38:33 +0700

    Bug Fixing

--- unitTest/view/propertiesView.js
+++ unitTest/view/propertiesView.js
@@ -1,25 +1,26 @@
+import { readFileSync } from 'fs';
 import { sheetDoc } from "../queryData/sheetDoc.js";
+const ciceroKey = JSON.parse (readFileSync('ciceroKey.json'));
 
 export async function propertiesView(clientName, type) {
+  
+      const userDoc = await sheetDoc(ciceroKey.dbKey.userDataID, clientName);
+      const userRows = userDoc.data;
 
-    const userDoc = await sheetDoc(ciceroKey.dbKey.userDataID, clientName);
-    const userRows = userDoc.data;
+      let dataString;
 
-
-    let dataString;
-
-    //Collect Divisi List String
-    for (let i = 0; i < userRows.length; i++) {
-      if (!dataList.includes(userRows[i].get(type))) {
-        dataString = dataString+"\n"+userRows[i].get(type);
+      //Collect Divisi List String
+      for (let i = 0; i < userRows.length; i++) {
+        if (!dataList.includes(userRows[i].get(type))) {
+          dataString = dataString+"\n"+userRows[i].get(type);
+        }
       }
-    }
 
-    let data = {
-    data : `*`+type+` List*\n\n`+dataString,
-    state: true,
-    code: 201
-    }
+      let data = {
+        data : `*`+type+` List*\n\n`+dataString,
+        state: true,
+        code: 201
+      }
 
-    return data;
+      return data;
 }
