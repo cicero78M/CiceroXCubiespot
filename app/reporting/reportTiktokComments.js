410f6b28dca6a4be4b149fbcbbb83a188665abf9
papiqo <nationalite45@gmail.com>
2024-12-29 14:38:35 +0700

    Update

--- app/reporting/reportTiktokComments.js
+++ app/reporting/reportTiktokComments.js
@@ -2,47 +2,32 @@ import { readFileSync } from 'fs';
 import { sheetDoc as _sheetDoc } from '../database_query/sheetDoc.js';
 import { clientData } from '../database_query/clientData.js';
 import { listValueData } from '../database_query/listValueData.js';
-
 const ciceroKey = JSON.parse (readFileSync('ciceroKey.json'));
-
 export async function reportTiktokComments(clientName) {
   try {
-
     console.log("Report Tiktok Function Executed");
-
     const d = new Date();
     const localDate = d.toLocaleDateString('id');
     const localHours = d.toLocaleTimeString('id');
-
     const clientResponse = await clientData(clientName);
-
     if (clientResponse.state) {
-
       // If Client_ID exist. then get official content
       if (clientResponse.data.isClientID && clientResponse.data.isStatus) {
-    
           let divisiResponse = await listValueData(clientName, 'DIVISI');
           let divisiList = divisiResponse.data;
-
           let userDoc = await _sheetDoc(ciceroKey.dbKey.userDataID, clientName);
           let userRows = userDoc.data;
-
           var userAll = 0;
-
           for (let i = 0; i < userRows.length; i++) {
             if (userRows[i].get('STATUS') === 'TRUE' ){
               userAll++;
             }
           }
-
           //Collect Shortcode from Database        
           let shortcodeList = [];
-
           const tiktokOfficialDoc = await _sheetDoc(ciceroKey.dbKey.tiktokOfficialID, clientName);
           const tiktokOfficialRows = tiktokOfficialDoc.data;
-
           let shortcodeListString = '';
-
           for (let i = 0; i < tiktokOfficialRows.length; i++) {
             let itemDate = new Date(tiktokOfficialRows[i].get('TIMESTAMP') * 1000);
             if (itemDate.toLocaleDateString('id') === localDate) {
@@ -52,13 +37,10 @@ export async function reportTiktokComments(clientName) {
               }
             }
           }
-
           if (shortcodeList.length >= 1) {
-
             let tiktokUsernameDoc = await _sheetDoc(ciceroKey.dbKey.tiktokCommentUsernameID, clientName);
             let tiktokCommentsUsernameRows = tiktokUsernameDoc.data;
             let userCommentData = [];
-
             for (let i = 0; i < shortcodeList.length; i++) {
               //code on the go
               for (let ii = 0; ii < tiktokCommentsUsernameRows.length; ii++) {
@@ -74,10 +56,8 @@ export async function reportTiktokComments(clientName) {
                 }
               }
             }
-
             let userNotComment = [];
             let notCommentList = [];
-
             for (let iii = 0; iii < userRows.length; iii++) {
               if (!userCommentData.includes(userRows[iii].get('TIKTOK').replaceAll('@', ''))) {
                 if (!userNotComment.includes(userRows[iii].get('ID_KEY'))) {
@@ -90,15 +70,11 @@ export async function reportTiktokComments(clientName) {
                 }
               }
             }
-
             let dataTiktok = '';
             let userCounter = 0;
-
             for (let iii = 0; iii < divisiList.length; iii++) {
-
               let divisiCounter = 0;
               let userByDivisi = '';
-
               for (let iv = 0; iv < notCommentList.length; iv++) {
                 if (divisiList[iii] === notCommentList[iv].get('DIVISI')) {
                   userByDivisi = userByDivisi.concat('\n' + notCommentList[iv].get('TITLE') + ' ' + notCommentList[iv].get('NAMA') + ' - ' + notCommentList[iv].get('TIKTOK'));
@@ -106,15 +82,12 @@ export async function reportTiktokComments(clientName) {
                   userCounter++;
                 }
               }
-
               if (divisiCounter != 0) {
                 dataTiktok = dataTiktok.concat('\n\n*' + divisiList[iii] + '* : ' + divisiCounter + ' User\n' + userByDivisi);
               }
             }
-
             let tiktokSudah = userAll - notCommentList.length;
             let responseData;
-
             if (clientResponse.data.isClientType === "RES") {
               responseData = {
                 data: "Mohon Ijin Komandan,\n\nMelaporkan Rekap Pelaksanaan Komentar dan Likes Pada " + shortcodeList.length + " Konten dari akun Resmi Tik Tok *POLRES " + clientName
@@ -146,7 +119,6 @@ export async function reportTiktokComments(clientName) {
             return responseData;
           }
       } else {
-
         let responseData = {
           data: 'Your Client ID has Expired, Contacts Developers for more Informations',
           state: true,
@@ -155,9 +127,7 @@ export async function reportTiktokComments(clientName) {
         console.log(responseData.data);
         return responseData;
       }
-
     } else {
-
       let responseData = {
         data: 'Client Name Doesn\'t Exist',
         state: false,
@@ -166,7 +136,6 @@ export async function reportTiktokComments(clientName) {
       console.log(responseData.data);
       return responseData;
     }
-
   } catch (error) {
     let responseData = {
       data: error,
