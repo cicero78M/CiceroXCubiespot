a1717db8ac39ea32dbe7e32cdbcb14b32339e263
papiqo <nationalite45@gmail.com>
2024-12-31 12:21:44 +0700

    Update

--- app/scrapping/collectFolllowingInsta.js
+++ app/scrapping/collectFolllowingInsta.js
@@ -1,11 +1,11 @@
 import { readFileSync } from "fs";
-import { instaUserAPI } from "../socialMediaAPI/instaAPI.js";
+import { instaFollowingAPI } from "../socialMediaAPI/instaAPI.js";
 
 const ciceroKey = JSON.parse (readFileSync('ciceroKey.json'));
 
 export async function collectFollowing(username) {
-    
-    let responseData = await instaUserAPI(username, ciceroKey.instaKey.instaFollowing);
+
+    let responseData = await instaFollowingAPI(username, ciceroKey.instaKey.instaFollowing);
 
     console.log(responseData);
 
