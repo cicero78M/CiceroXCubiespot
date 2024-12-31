c1e5f1d46aa29defe14d855de51b18e3da336e44
papiqo <nationalite45@gmail.com>
2024-12-31 13:42:47 +0700

    Update

--- app/scrapping/collectFolllowingInsta.js
+++ app/scrapping/collectFolllowingInsta.js
@@ -1,13 +1,30 @@
+import { googleAuth } from "../database_query/sheetDoc.js";
 import { instaInfoAPI } from "../socialMediaAPI/instaAPI.js";
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 
-export async function collectFollowing(username) {
 
-    let responseData = await instaInfoAPI(username);
+export async function collectFollowing(from, username) {
+    
+    const instaProfileDoc = new GoogleSpreadsheet(ciceroKey.dbKey.instaProfileData, googleAuth); //Google Authentication for InstaOfficial DB
+   
+    await instaProfileDoc.loadInfo(); // loads document properties and worksheets
+    const instaProfileSheet = instaProfileDoc.sheetsByTitle["PROFILE"];
 
-    console.log(responseData);
+    instaInfoAPI(username).then(async (responseInfo) => {
+        instaProfileSheet.addRow({
+            WHATSAPP: from, USERNAME: username, isPRIVATE:responseInfo.data.is_private, isBUSSINESS:responseInfo.data.is_business, isVERIFIED:responseInfo.data.is_verified,
+            CATEGORY:responseInfo.data.category, CONTACT:responseInfo.data.contact_phone_number, EMAIL:responseInfo.data.public_email, FULL_NAME:responseInfo.data.full_name,	
+            FOLLOWER:responseInfo.data.follower_count, FOLLOWING:responseInfo.data.following_count, MEDIA_COUNT:responseInfo.data.media_count,
+            BIOGRAPHY:responseInfo.data.biography
+        });
+        console.log(responseInfo);
+
+        
+    });
+
 
     let data = {
-        data: responseData,
+        data: responseInfo.data,
         code: 200,
         state: false
     };
