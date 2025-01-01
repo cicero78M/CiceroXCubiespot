f7bc83a31de24511664925ae5cb6b64c95f59dd5
papiqo <nationalite45@gmail.com>
2025-01-01 17:39:14 +0700

    Update

--- app/database/secuidTiktok.js
+++ app/database/secuidTiktok.js
@@ -8,7 +8,11 @@ export async function setSecuid(clientValue) {
     try {
 
         const clientName = clientValue.get('CLIENT_ID');
-
+        //Collect Content Shortcode from Official Account
+        let tiktokAccount = clientValue.get('TIKTOK');
+        let responseInfo = await tiktokUserInfoAPI(tiktokAccount.replaceAll('@', ''));
+        const secUid = await responseInfo.data.userInfo.user.secUid;
+        console.log(secUid);
         const clientDoc = new GoogleSpreadsheet(ciceroKey.dbKey.clientDataID, googleAuth); //Google Auth
         await clientDoc.loadInfo(); // loads document properties and worksheets
 
@@ -19,13 +23,6 @@ export async function setSecuid(clientValue) {
 
         for (let i = 0; i < clientRows.length; i++) {
             if (clientRows[i].get('CLIENT_ID') === clientName) {
-
-                //Collect Content Shortcode from Official Account
-                let tiktokAccount = clientValue.get('TIKTOK');
-
-                let responseInfo = await tiktokUserInfoAPI(tiktokAccount.replaceAll('@', ''));
-                const secUid = await responseInfo.data.userInfo.user.secUid;
-
                 isClient = true;
                 clientRows[i].assign({ SECUID: secUid });; // Updae State Value
                 await clientRows[i].save(); //save update
