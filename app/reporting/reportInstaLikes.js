76e5ffd54ea742f2c0db6130b5b87c0df1d14dd6
papiqo <nationalite45@gmail.com>
2025-01-01 10:34:09 +0700

    Update

--- app/reporting/reportInstaLikes.js
+++ app/reporting/reportInstaLikes.js
@@ -82,11 +82,10 @@ export async function reportInstaLikes(clientName) {
         
         let UserNotLikes = [];
         let notLikesList = [];
+        if (clientResponse.data.isClientType === "RES") {
 
-        for (let iii = 0; iii < userRows.length; iii++) {
+          for (let iii = 0; iii < userRows.length; iii++) {
             if (!userLikesData.includes(userRows[iii].get('INSTA'))) {
-              console.log(userRows[iii]);
-
               if (!UserNotLikes.includes(userRows[iii].get('ID_KEY'))) {
                 if (userRows[iii].get('STATUS') === 'TRUE' ){
                   if (userRows[iii].get('EXCEPTION') === "FALSE"){
@@ -96,7 +95,27 @@ export async function reportInstaLikes(clientName) {
                 }
               }
             }          
-        }
+          }
+
+        } else if (clientResponse.data.isClientType === "COM") {
+
+          
+          for (let iii = 0; iii < userRows.length; iii++) {
+            if (!userLikesData.includes(userRows[iii].get('INSTA'))) {
+              console.log(userRows[iii].get('INSTA'));
+              if (!UserNotLikes.includes(userRows[iii].get('ID_KEY'))) {
+                if (userRows[iii].get('STATUS') === 'TRUE' ){
+                  if (userRows[iii].get('EXCEPTION') === "FALSE"){
+                    UserNotLikes.push(userRows[iii].get('ID_KEY'));
+                    notLikesList.push(userRows[iii]);
+                  }
+                }
+              }
+            }          
+          }
+
+        } 
+        
 
         let dataInsta = '';
         let userCounter = 0;
