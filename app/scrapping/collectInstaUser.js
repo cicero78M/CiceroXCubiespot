37d9075d0aa1be6780b82e3b2704cae07b47e3cd
papiqo <nationalite45@gmail.com>
2025-01-02 13:23:30 +0700

    Update

--- app/scrapping/collectInstaUser.js
+++ app/scrapping/collectInstaUser.js
@@ -20,14 +20,14 @@ export async function instaUserData(from, username) {
 
 
         if (!isDataExist){
-            
+
             let responseInfo = await instaInfoAPI(username);
 
             instaProfileSheet.addRow({
                 WHATSAPP: from, USERNAME: username, isPRIVATE:responseInfo.data.data.is_private, isBUSSINESS:responseInfo.data.data.is_business, isVERIFIED:responseInfo.data.data.is_verified,
                 CATEGORY:responseInfo.data.data.category, CONTACT:responseInfo.data.data.contact_phone_number, EMAIL:responseInfo.data.data.public_email, FULL_NAME:responseInfo.data.data.full_name,	
                 FOLLOWER:responseInfo.data.data.follower_count, FOLLOWING:responseInfo.data.data.following_count, MEDIA_COUNT:responseInfo.data.data.media_count,
-                BIOGRAPHY:responseInfo.data.data.biography, isFollowing : isFollowing
+                BIOGRAPHY:responseInfo.data.data.biography, isFOLLOWING : isFollowing
             });
 
             if (isFollowing){
