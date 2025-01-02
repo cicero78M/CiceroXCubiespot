b258b2c956067cd9979e24d43ba434c16e4a8950
papiqo <nationalite45@gmail.com>
2025-01-02 15:01:16 +0700

    Update

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ client.on('ready', () => {
     });
 
     // Reload Tiktok every hours until 15/18/21
-    schedule('00 15,18,21 * * *', async () => {
+    schedule('02 15,18,21 * * *', async () => {
         //Date Time
         const d = new Date();
         const localDate = d.toLocaleDateString("en-US", {
