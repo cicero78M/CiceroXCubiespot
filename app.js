f4d4044274e80758039a5986e47b6696b958872c
papiqo <nationalite45@gmail.com>
2025-01-02 10:47:16 +0700

    Update

--- app.js
+++ app.js
@@ -807,9 +807,8 @@ client.on('message', async (msg) => {
                                     const instaLink = splittedMsg[1].split('?')[0];
                                     const instaUsername = instaLink.replaceAll('/profilecard/','').split('/').pop();  
 
-                                    const responseData = await collectInstaUser(msg.from, instaUsername);
-                                    client.sendMessage(msg.from, responseData.data);
-                                    
+                                    collectInstaUser(msg.from, instaUsername).then(async (responseData) => {
+                                        console.log(responseData);                                    });
                                 } else {
                                     client.sendMessage(msg.from, "Silahkan Cek Kembali, link yang anda cantumkan, pastikan link tersebut adalah link Akun Profile " 
                                         +"Instagram anda dan bukan Akun Private.\n\nTerimakasih.");
