74632391bb5c686afe1e4918cf70838436b60914
papiqo <nationalite45@gmail.com>
2025-01-02 12:06:47 +0700

    Update

--- app/scrapping/collectInstaFollow.js
+++ app/scrapping/collectInstaFollow.js
@@ -12,22 +12,19 @@ export async function instaUserFollowing(username, from) {
         const following_count = instaUser.data.data.data.following_count;
         console.log(following_count);
 
-        while (following_count >= user_counter ) {
+        do {
             console.log("Starting...");
             let instaFollowing = await instaFollowingAPI(username, pages);
             userList = await instaFollowing.data.data.items;
             pages = await instaFollowing.data.pagination_token;
-            user_counter = user_counter+userList.length;
-            console.log(user_counter);
             console.log(pages);
             for (let i = 0; i < userList.length; i++){
-                user_counter++;
-                if (userList[i].username = username){
+                    if (userList[i].username = username){
                     isFollowing = true;
                     break;
                 }
             }            
-        }
+        } while (pages != null);
 
         if(isFollowing){
             let responseData = {
