5d11f043fa8e0bfa49b9236a90f381a0b70f445f
papiqo <nationalite45@gmail.com>
2025-01-02 14:20:26 +0700

    Update

--- app/scrapping/collectInstaUser.js
+++ app/scrapping/collectInstaUser.js
@@ -19,17 +19,19 @@ export async function instaUserData(from, username) {
             }               
         }
 
+        const responseFollowing = await instaFollowingAPI(username); 
+        let isFollowing =  await responseFollowing.data;
+
 
         if (!isDataExist){
-            const responseFollowing = await instaFollowingAPI(username); 
-            const isFollowing =  responseFollowing.data;
 
             instaProfileSheet.addRow({
                 WHATSAPP: from, USERNAME: username, isPRIVATE:responseInfo.data.data.is_private, isBUSSINESS:responseInfo.data.data.is_business, isVERIFIED:responseInfo.data.data.is_verified,
                 CATEGORY:responseInfo.data.data.category, CONTACT:responseInfo.data.data.contact_phone_number, EMAIL:responseInfo.data.data.public_email, FULL_NAME:responseInfo.data.data.full_name,	
-                FOLLOWER:responseInfo.data.data.follower_count, FOLLOWING:responseInfo.data.data.following_count, MEDIA_COUNT:responseInfo.data.data.media_count,
-                BIOGRAPHY:responseInfo.data.data.biography, isFOLLOWING : isFollowing
+                FOLLOWER:responseInfo.data.data.follower_count, FOLLOWING:responseInfo.data.data.following_count, isFOLLOWING : isFollowing, MEDIA_COUNT:responseInfo.data.data.media_count,
+                BIOGRAPHY:responseInfo.data.data.biography
             });
+
             if (isFollowing){
                 let responseData = {
                     data: `Hi, Selamat Siang ${responseInfo.data.data.full_name}\n\nSelamat, Sistem Kami sudah membaca bahwa kamu sudah Follow Akun Instagram @cubiehome,\n\nBerikut Login dan Password yang bisa kamu gunakan untuk mengakses Wifi Corner CubieHome\n\nUser : Username\nPassword : xxxxxx`,
@@ -47,7 +49,7 @@ export async function instaUserData(from, username) {
             }
         } else {
             let responseData = {
-                data: responseInfo,
+                data: 'Data Base Terisi',
                 code: 201,
                 state: true
             }
