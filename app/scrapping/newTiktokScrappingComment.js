4586ae0c04982ef851b236170a0c2d8dd8d7cab2
papiqo <nationalite45@gmail.com>
2025-01-03 12:08:58 +0700

    Update

--- app/scrapping/newTiktokScrappingComment.js
+++ app/scrapping/newTiktokScrappingComment.js
@@ -21,7 +21,7 @@ export async function getLikesTiktok(todayItems, cursorNumber) {
 
             if (responseComments.data.has_more === 1){    
 
-                console.log(responseComments.data.data.cursor );
+                console.log(responseComments.data.cursor );
 
                 setTimeout(() => {
                     console.log('next data');
