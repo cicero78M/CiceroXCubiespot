e104d4f7622c37b4868dfe9ff646cda527dbf0e8
papiqo <nationalite45@gmail.com>
2025-01-03 10:36:02 +0700

    Update

--- app/scrapping/newTiktokEngagements.js
+++ app/scrapping/newTiktokEngagements.js
@@ -3,7 +3,7 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { tiktokPostAPI } from '../socialMediaAPI/tiktokAPI.js';
 import { client } from '../../app.js';
 import { ciceroKey, googleAuth } from '../database_query/sheetDoc.js';
-import { getLikesTiktok } from './newTiktokScarppingComment.js';
+import { getLikesTiktok } from './newTiktokScrappingComment';
 
 export async function newCollectTiktokComments(clientValue) {
   //Date Time
