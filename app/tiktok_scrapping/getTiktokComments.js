675eb92e42fdb7ed3e6e45461dff5da89e466550
papiqo <nationalite45@gmail.com>
2025-01-03 22:36:35 +0700

    Update

--- app/tiktok_scrapping/getTiktokComments.js
+++ app/tiktok_scrapping/getTiktokComments.js
@@ -14,8 +14,9 @@ export async function getTiktokComments(clientName, todayItems) {
 
         async function forLoopGetComments(items, cursorNumber) {
 
-            tiktokCommentAPI(items, cursorNumber).then (async responseComments =>{
-                let commentItems = responseComments.data.comments;
+            console.log(items);
+            tiktokCommentAPI(items, cursorNumber).then (async response =>{
+                let commentItems = response.data.comments;
                 for (let ii = 0; ii < commentItems.length; ii++) {
                     if (commentItems[ii].user.unique_id != undefined || commentItems[ii].user.unique_id != null || commentItems[ii].user.unique_id != "") {
                         if (!newDataUsers.includes(commentItems[ii].user.unique_id)) {
@@ -25,21 +26,21 @@ export async function getTiktokComments(clientName, todayItems) {
                 }
     
                 if (switchNumber = 0){
-                    if (responseComments.data.has_more === 1){    
-                        console.log(responseComments.data.cursor );
+                    if (response.data.has_more === 1){    
+                        console.log(response.data.cursor );
                         setTimeout(async() => {
                             console.log('next data');
-                            forLoopGetComments(items, responseComments.data.cursor);
+                            forLoopGetComments(items, response.data.cursor);
             
                         }, 2000);
             
                     } else {
-                        console.log(responseComments.data.cursor );
-                        if(responseComments.data.total > 400){
-                            if (responseComments.data.cursor < responseComments.data.total){
+                        console.log(response.data.cursor );
+                        if(response.data.total > 400){
+                            if (response.data.cursor < response.data.total){
                                 setTimeout(async () => {
                                     console.log('next data');
-                                    forLoopGetComments(items, responseComments.data.cursor);
+                                    forLoopGetComments(items, response.data.cursor);
                                 }, 2000);
                             } else {
                                 switchNumber = 1;
@@ -48,7 +49,7 @@ export async function getTiktokComments(clientName, todayItems) {
                         } else {
                             setTimeout(async () => {
                                 console.log('next data');
-                                forLoopGetComments(items, responseComments.data.cursor);
+                                forLoopGetComments(items, response.data.cursor);
                             }, 2000);                    
                         }
                     }
