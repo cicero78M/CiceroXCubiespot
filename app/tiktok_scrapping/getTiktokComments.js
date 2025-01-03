c1c6a8c780888c26bcf2e3f88d35be6b78262dd7
papiqo <nationalite45@gmail.com>
2025-01-03 22:24:13 +0700

    Update

--- app/tiktok_scrapping/getTiktokComments.js
+++ app/tiktok_scrapping/getTiktokComments.js
@@ -1,50 +1,63 @@
 import { tiktokCommentAPI } from "../socialMediaAPI/tiktokAPI.js";
 
-export async function getTiktokComments(todayItems, cursorNumber) {
+export async function getTiktokComments(clientName, todayItems) {
 
-    console.log('Exec 1');
-
+    console.log('Exec Get Tiktok Comments');
+    let cursorNumber = 0;
+    let switchNumber = 0;
     let newDataUsers = [];    
+    
+    for (let i = 0; i < todayItems.length; i++) {
 
-    async function forLoopGetComments(todayItems, cursorNumber) {
-        tiktokCommentAPI(todayItems, cursorNumber).then (responseComments =>{
-            let commentItems = responseComments.data.comments;
-            for (let ii = 0; ii < commentItems.length; ii++) {
-                if (commentItems[ii].user.unique_id != undefined || commentItems[ii].user.unique_id != null || commentItems[ii].user.unique_id != "") {
-                    if (!newDataUsers.includes(commentItems[ii].user.unique_id)) {
-                        newDataUsers.push(commentItems[ii].user.unique_id);
+        forLoopGetComments(todayItems[i], cursorNumber);   
+
+        async function forLoopGetComments(items, cursorNumber) {
+
+            tiktokCommentAPI(items, cursorNumber).then (async responseComments =>{
+                let commentItems = responseComments.data.comments;
+                for (let ii = 0; ii < commentItems.length; ii++) {
+                    if (commentItems[ii].user.unique_id != undefined || commentItems[ii].user.unique_id != null || commentItems[ii].user.unique_id != "") {
+                        if (!newDataUsers.includes(commentItems[ii].user.unique_id)) {
+                            newDataUsers.push(commentItems[ii].user.unique_id);
+                        }
                     }
                 }
-            }
     
-            if (responseComments.data.has_more === 1){    
-                console.log(responseComments.data.cursor );
-                setTimeout(async() => {
-//                    console.log('next data');
-                    forLoopGetComments(todayItems, responseComments.data.cursor);
-    
-                }, 2000);
-    
-            } else {
-//                console.log(responseComments.data.cursor );
-                if(responseComments.data.total > 400){
-                    if (responseComments.data.cursor < responseComments.data.total){
-                        setTimeout(async () => {
+                if (switchNumber = 0){
+                    if (responseComments.data.has_more === 1){    
+                        console.log(responseComments.data.cursor );
+                        setTimeout(async() => {
                             console.log('next data');
-                            forLoopGetComments(todayItems, responseComments.data.cursor);
+                            forLoopGetComments(items, responseComments.data.cursor);
+            
                         }, 2000);
+            
                     } else {
-                        console.log(newDataUsers.length);
+                        console.log(responseComments.data.cursor );
+                        if(responseComments.data.total > 400){
+                            if (responseComments.data.cursor < responseComments.data.total){
+                                setTimeout(async () => {
+                                    console.log('next data');
+                                    forLoopGetComments(items, responseComments.data.cursor);
+                                }, 2000);
+                            } else {
+                                switchNumber = 1;
+                                postTiktokUserComments(clientName, items, newDataUsers)
+                            }
+                        } else {
+                            setTimeout(async () => {
+                                console.log('next data');
+                                forLoopGetComments(items, responseComments.data.cursor);
+                            }, 2000);                    
+                        }
                     }
                 } else {
-                    console.log(newDataUsers.length);
+                    postTiktokUserComments(clientName, items, newDataUsers);
                 }
-            }
-        
-        }). catch (response => {
-            console.log(response);
-        });
+                    
+            }). catch (response => {
+                console.log(response);
+            });
+        }
     }
-
-    await forLoopGetComments(todayItems, cursorNumber);
 }
---
+++ app/tiktok_scrapping/postTiktokUserComments.js
@@ -0,0 +1,47 @@
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { ciceroKey, googleAuth } from "../database_query/sheetDoc";
+
+
+export async function postTiktokUserComments(clientName, items, newDataUsers) {
+
+    let dataCleaning = [];
+    let hasShortcode = false;
+
+    const tiktokCommentsUsernameDoc = new GoogleSpreadsheet(ciceroKey.dbKey.tiktokCommentUsernameID, googleAuth); //Google Authentication for instaLikes Username DB
+    await  tiktokCommentsUsernameDoc.loadInfo(); // loads document properties and worksheets            
+    let tiktokCommentsUsernameSheet = tiktokCommentsUsernameDoc.sheetsByTitle[clientName];
+    tiktokCommentsUsernameSheet.getRows().then (response =>{
+
+        for (let ii = 0; ii < response.length; ii++) {
+            if (response[ii].get('SHORTCODE') === items) {
+                hasShortcode = true;
+                const fromRows = Object.values(response[ii].toObject());
+
+                for (let iii = 0; iii < fromRows.length; iii++) {
+                    if (fromRows[iii] !== undefined || fromRows[iii] !== null || fromRows[iii] !== "") {
+                        if (!newDataUsers.includes(fromRows[iii])) {
+                            newDataUsers.push(fromRows[iii]);
+                        }
+                    }
+                }
+
+                for (let iv = 0; iv < newDataUsers.length; iv++) {
+                    if (newDataUsers[iv] != '' || newDataUsers[iv] != null || newDataUsers[iv] != undefined) {
+                        if (!dataCleaning.includes(newDataUsers[iv])) {
+                            dataCleaning.push(newDataUsers[iv]);
+                        }
+                    }
+                }
+        
+                console.log(clientName + ' Update Data');
+
+
+                response[ii].delete();
+                tiktokCommentsUsernameSheet.addRow(dataCleaning);
+                updateData++; 
+                
+                console.log("done");
+            }
+        }
+    });  
+}
