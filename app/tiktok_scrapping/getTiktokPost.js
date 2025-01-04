9e1acb421a621f378d9e143619b2e64c3c1135e9
papiqo <nationalite45@gmail.com>
2025-01-04 19:17:43 +0700

    Update

--- app/tiktok_scrapping/getTiktokPost.js
+++ app/tiktok_scrapping/getTiktokPost.js
@@ -34,73 +34,77 @@ export async function getTiktokPost(clientValue) {
                 await tiktokPostAPI(secUid, cursor).then(async  response =>{
                     items = await response.data.data.itemList;
                     console.log(items);
-                    if (Array.isArray(items)) {
-                        for (let i = 0; i < items.length; i++) {
-                            let itemDate = new Date(items[i].createTime * 1000);
-                            if (itemDate.toLocaleDateString("en-US", {timeZone: "Asia/Jakarta"}) === localDate) {
-                                hasContent = true;
-                                itemByDay.push(items[i]);
-                                todayItems.push(items[i].video.id);
-                            }
-                                        
-                            if (hasContent) {
-    
-                                const tiktokOfficialDoc = new GoogleSpreadsheet(ciceroKey.dbKey.tiktokOfficialID, googleAuth); //Google Authentication for InstaOfficial DB
-                                await tiktokOfficialDoc.loadInfo(); // loads document properties and worksheets    
-                                const officialTiktokSheet = tiktokOfficialDoc.sheetsByTitle[clientName];
-                                officialTiktokSheet.getRows().then(response =>{
-    
-                                    for (let i = 0; i < response.length; i++) {
-                                        if (!shortcodeList.includes(response[i].get('SHORTCODE'))) {
-                                            shortcodeList.push(response[i].get('SHORTCODE'));
-                                        }
+
+                    for (let i = 0; i < items.length; i++) {
+                
+                        let itemDate = new Date(items[i].createTime * 1000);
+                        
+                        console.log(itemDate);                
+                        if (itemDate.toLocaleDateString("en-US", {timeZone: "Asia/Jakarta"}) === localDate) {
+                
+                            hasContent = true;
+                            itemByDay.push(items[i]);
+                            todayItems.push(items[i].video.id);
+                        }
+                                    
+                        if (hasContent) {
+
+                            const tiktokOfficialDoc = new GoogleSpreadsheet(ciceroKey.dbKey.tiktokOfficialID, googleAuth); //Google Authentication for InstaOfficial DB
+                            await tiktokOfficialDoc.loadInfo(); // loads document properties and worksheets    
+                            const officialTiktokSheet = tiktokOfficialDoc.sheetsByTitle[clientName];
+                            officialTiktokSheet.getRows().then(response =>{
+
+                                for (let i = 0; i < response.length; i++) {
+                                    if (!shortcodeList.includes(response[i].get('SHORTCODE'))) {
+                                        shortcodeList.push(response[i].get('SHORTCODE'));
                                     }
-    
+                                }
+
+                                for (let i = 0; i < itemByDay.length; i++) {
+                                    if (shortcodeList.includes(itemByDay[i].video.id)) {
+                                        hasShortcode = true;
+                                    }
+                                }
+
+                                if (hasShortcode) {
                                     for (let i = 0; i < itemByDay.length; i++) {
-                                        if (shortcodeList.includes(itemByDay[i].video.id)) {
-                                            hasShortcode = true;
-                                        }
-                                    }
-    
-                                    if (hasShortcode) {
-                                        for (let i = 0; i < itemByDay.length; i++) {
-                                            for (let ii = 0; ii < response.length; ii++) {
-                                                if (response[ii].get('SHORTCODE') === itemByDay[i].video.id) {
-                                                    response[ii].assign({
-                                                        TIMESTAMP: itemByDay[i].createTime, USER_ACCOUNT: itemByDay[i].author.uniqueId,
-                                                        SHORTCODE: itemByDay[i].video.id, ID: itemByDay[i].id, CAPTION: itemByDay[i].desc, COMMENT_COUNT: itemByDay[i].statsV2.commentCount,
-                                                        LIKE_COUNT: itemByDay[i].statsV2.diggCount, PLAY_COUNT: itemByDay[i].statsV2.playCount, COLLECT_COUNT: itemByDay[i].statsV2.collectCount,
-                                                        SHARE_COUNT: itemByDay[i].statsV2.shareCount, REPOST_COUNT: itemByDay[i].statsV2.repostCount
-                                                    }); // Jabatan Divisi Value
-                                                    response[ii].save(); //save update
-                                                    shortcodeUpdateCounter++;
-                                                } else if (!shortcodeList.includes(itemByDay[i].video.id)) {
-                                                    shortcodeList.push(itemByDay[i].video.id);   
-                                                    officialTiktokSheet.addRow({
-                                                        TIMESTAMP: itemByDay[i].createTime, USER_ACCOUNT: itemByDay[i].author.uniqueId,
-                                                        SHORTCODE: itemByDay[i].video.id, ID: itemByDay[i].id, CAPTION: itemByDay[i].desc, COMMENT_COUNT: itemByDay[i].statsV2.commentCount,
-                                                        LIKE_COUNT: itemByDay[i].statsV2.diggCount, PLAY_COUNT: itemByDay[i].statsV2.playCount, COLLECT_COUNT: itemByDay[i].statsV2.collectCount,
-                                                        SHARE_COUNT: itemByDay[i].statsV2.shareCount, REPOST_COUNT: itemByDay[i].statsV2.repostCount
-                                                    });
-                                                    shortcodeNewCounter++;
-                                                }
+                                        for (let ii = 0; ii < response.length; ii++) {
+                                            if (response[ii].get('SHORTCODE') === itemByDay[i].video.id) {
+                                                response[ii].assign({
+                                                    TIMESTAMP: itemByDay[i].createTime, USER_ACCOUNT: itemByDay[i].author.uniqueId,
+                                                    SHORTCODE: itemByDay[i].video.id, ID: itemByDay[i].id, CAPTION: itemByDay[i].desc, COMMENT_COUNT: itemByDay[i].statsV2.commentCount,
+                                                    LIKE_COUNT: itemByDay[i].statsV2.diggCount, PLAY_COUNT: itemByDay[i].statsV2.playCount, COLLECT_COUNT: itemByDay[i].statsV2.collectCount,
+                                                    SHARE_COUNT: itemByDay[i].statsV2.shareCount, REPOST_COUNT: itemByDay[i].statsV2.repostCount
+                                                }); // Jabatan Divisi Value
+                                                response[ii].save(); //save update
+                                                shortcodeUpdateCounter++;
+                                            } else if (!shortcodeList.includes(itemByDay[i].video.id)) {
+                                                shortcodeList.push(itemByDay[i].video.id);   
+                                                officialTiktokSheet.addRow({
+                                                    TIMESTAMP: itemByDay[i].createTime, USER_ACCOUNT: itemByDay[i].author.uniqueId,
+                                                    SHORTCODE: itemByDay[i].video.id, ID: itemByDay[i].id, CAPTION: itemByDay[i].desc, COMMENT_COUNT: itemByDay[i].statsV2.commentCount,
+                                                    LIKE_COUNT: itemByDay[i].statsV2.diggCount, PLAY_COUNT: itemByDay[i].statsV2.playCount, COLLECT_COUNT: itemByDay[i].statsV2.collectCount,
+                                                    SHARE_COUNT: itemByDay[i].statsV2.shareCount, REPOST_COUNT: itemByDay[i].statsV2.repostCount
+                                                });
+                                                shortcodeNewCounter++;
                                             }
                                         }
-    
-                                    } else {
-                                        //Push New Shortcode Content to Database
-                                        for (let i = 0; i < itemByDay.length; i++) {
-                                            officialTiktokSheet.addRow({
-                                                TIMESTAMP: itemByDay[i].createTime, USER_ACCOUNT: itemByDay[i].author.uniqueId,
-                                                SHORTCODE: itemByDay[i].video.id, ID: itemByDay[i].id, CAPTION: itemByDay[i].desc, COMMENT_COUNT: itemByDay[i].statsV2.commentCount,
-                                                LIKE_COUNT: itemByDay[i].statsV2.diggCount, PLAY_COUNT: itemByDay[i].statsV2.playCount, COLLECT_COUNT: itemByDay[i].statsV2.collectCount,
-                                                SHARE_COUNT: itemByDay[i].statsV2.shareCount, REPOST_COUNT: itemByDay[i].statsV2.repostCount
-                                            });
-                                            shortcodeNewCounter++;
-                                        }
                                     }
-                                });
 
+                                } else {
+                                    //Push New Shortcode Content to Database
+                                    for (let i = 0; i < itemByDay.length; i++) {
+                                        officialTiktokSheet.addRow({
+                                            TIMESTAMP: itemByDay[i].createTime, USER_ACCOUNT: itemByDay[i].author.uniqueId,
+                                            SHORTCODE: itemByDay[i].video.id, ID: itemByDay[i].id, CAPTION: itemByDay[i].desc, COMMENT_COUNT: itemByDay[i].statsV2.commentCount,
+                                            LIKE_COUNT: itemByDay[i].statsV2.diggCount, PLAY_COUNT: itemByDay[i].statsV2.playCount, COLLECT_COUNT: itemByDay[i].statsV2.collectCount,
+                                            SHARE_COUNT: itemByDay[i].statsV2.shareCount, REPOST_COUNT: itemByDay[i].statsV2.repostCount
+                                        });
+                                        shortcodeNewCounter++;
+                                    }
+                                }
+
+                                    
                                 let data = {
                                     data:todayItems,
                                     state: true,
@@ -108,17 +112,30 @@ export async function getTiktokPost(clientValue) {
                                 }
 
                                 resolve (data);
-                     
-                            } else {
-                                let data = {
-                                        data: clientName + ' Tiktok Account Has No Content',
+                            }).catch(
+                                response =>{
+                                    let data = {
+                                        data: clientName + ' '+response,
                                         state: true,
-                                        code: 201
-                                };
-                                reject (data);
-                            }
+                                        code: 303
+                                    };
+                                    reject (data);
+
+                                }
+
+                            );
+
+                    
+                        } else {
+                            let data = {
+                                    data: clientName + ' Tiktok Account Has No Content',
+                                    state: true,
+                                    code: 201
+                            };
+                            reject (data);
                         }
                     }
+                    
                 });
             } else {
                 let data = {
