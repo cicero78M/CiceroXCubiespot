5d95de198bd5eccb0d9bddb3e2714850963a2ce3
papiqo <nationalite45@gmail.com>
2025-01-04 17:12:49 +0700

    Update

--- app/reporting/reportInstaLikes.js
+++ app/reporting/reportInstaLikes.js
@@ -83,7 +83,7 @@ export async function reportInstaLikes(clientValue) {
         let notLikesList = [];
 
         for (let i = 0; i < userRows.length; i++) {     
-          if (userRows[i].get('INSTA') !== undefined || userRows[i].get('INSTA') !== null || userRows[i].get('INSTA') !== ""){
+          if (userRows[i].get('INSTA') != undefined || userRows[i].get('INSTA') != null || userRows[i].get('INSTA') != ""){
             if (!userLikesData.includes(userRows[i].get('INSTA'))) {
               if (!UserNotLikes.includes(userRows[i].get('ID_KEY'))) {
                 if (userRows[i].get('STATUS') === 'TRUE' ){
