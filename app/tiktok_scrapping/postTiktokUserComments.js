2399ad88a5793b9bbde2e5258b52bd8ab931cb84
papiqo <nationalite45@gmail.com>
2025-01-04 20:09:04 +0700

    Update

--- app/tiktok_scrapping/postTiktokUserComments.js
+++ app/tiktok_scrapping/postTiktokUserComments.js
@@ -4,36 +4,55 @@ import { ciceroKey, googleAuth } from "../database_query/sheetDoc.js";
 let hasShortcode = false;
 
 export async function postTiktokUserComments(clientName, items, userdata) {
+
     return new Promise(async (resolve) => { 
         
-        const tiktokCommentsUsernameDoc = new GoogleSpreadsheet(ciceroKey.dbKey.tiktokCommentUsernameID, googleAuth); //Google Authentication for instaLikes Username DB
-        await  tiktokCommentsUsernameDoc.loadInfo(); // loads document properties and worksheets            
-        let tiktokCommentsUsernameSheet = tiktokCommentsUsernameDoc.sheetsByTitle[clientName];
-        await tiktokCommentsUsernameSheet.getRows().then ( async response =>{
-            for (let ii = 0; ii < response.length; ii++) {
-                if (response[ii].get('SHORTCODE') === items) {
-                    hasShortcode = true;
-                    const fromRows = Object.values(response[ii].toObject());
-                    console.log(fromRows);
-                    for (let iii = 0; iii < fromRows.length; iii++) {
-                        if (fromRows[iii] != undefined || fromRows[iii] != null || fromRows[iii] != "") {
-                            if (!userdata.includes(fromRows[iii])) {
-                                userdata.push(fromRows[iii]);
+        try {
+
+            const tiktokCommentsUsernameDoc = new GoogleSpreadsheet(ciceroKey.dbKey.tiktokCommentUsernameID, googleAuth); //Google Authentication for instaLikes Username DB
+            await  tiktokCommentsUsernameDoc.loadInfo(); // loads document properties and worksheets            
+            let tiktokCommentsUsernameSheet = tiktokCommentsUsernameDoc.sheetsByTitle[clientName];
+            await tiktokCommentsUsernameSheet.getRows().then ( async response =>{
+                for (let ii = 0; ii < response.length; ii++) {
+                    if (response[ii].get('SHORTCODE') === items) {
+                        hasShortcode = true;
+                        const fromRows = Object.values(response[ii].toObject());
+                        console.log(fromRows);
+
+                        for (let iii = 0; iii < fromRows.length; iii++) {
+                            if (fromRows[iii] != undefined || fromRows[iii] != null || fromRows[iii] != "") {
+                                
+                                if (!userdata.includes(fromRows[iii])) {
+                                    userdata.push(fromRows[iii]);
+                                }
                             }
                         }
+
+                        await response[ii].delete();
+                        await tiktokCommentsUsernameSheet.addRow(userdata);
+                 
+                        let data = {
+                                data: `${clientName} Adding Tiktok Username to ${items}`,
+                                state: true,
+                                code: 200
+                            };
+                 
+                        resolve (data);                
                     }
-                    await userdata[ii].delete();
-                    await tiktokCommentsUsernameSheet.addRow(userdata);
-             
-                    let data = {
-                            data: `${clientName} Adding Tiktok Username to ${items}`,
-                            state: true,
-                            code: 200
-                        };
-             
-                    resolve (data);                
                 }
-            }
-        });
+            });
+            
+        } catch (error) {
+
+            console.log(error);
+            let data = {
+                    data: error,
+                    state: true,
+                    code: 200
+                };
+        
+            resolve (data);
+        }
+
     }); 
 }
