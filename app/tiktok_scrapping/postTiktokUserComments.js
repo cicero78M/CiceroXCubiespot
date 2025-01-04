68f73153f0614c0659871303e56a30755b14d912
papiqo <nationalite45@gmail.com>
2025-01-04 11:21:27 +0700

    Update

--- app/tiktok_scrapping/postTiktokUserComments.js
+++ app/tiktok_scrapping/postTiktokUserComments.js
@@ -2,38 +2,37 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 import { ciceroKey, googleAuth } from "../database_query/sheetDoc.js";
 
 export async function postTiktokUserComments(clientName, items, newDataUsers) {
-
+    return new Promise(async (resolve) => { 
     
-    const tiktokCommentsUsernameDoc = new GoogleSpreadsheet(ciceroKey.dbKey.tiktokCommentUsernameID, googleAuth); //Google Authentication for instaLikes Username DB
-    await  tiktokCommentsUsernameDoc.loadInfo(); // loads document properties and worksheets            
-    let tiktokCommentsUsernameSheet = tiktokCommentsUsernameDoc.sheetsByTitle[clientName];
+        const tiktokCommentsUsernameDoc = new GoogleSpreadsheet(ciceroKey.dbKey.tiktokCommentUsernameID, googleAuth); //Google Authentication for instaLikes Username DB
+        await  tiktokCommentsUsernameDoc.loadInfo(); // loads document properties and worksheets            
+        let tiktokCommentsUsernameSheet = tiktokCommentsUsernameDoc.sheetsByTitle[clientName];
 
-    let hasShortcode = false;
+        let hasShortcode = false;
 
-    await tiktokCommentsUsernameSheet.getRows().then ( response =>{
+        await tiktokCommentsUsernameSheet.getRows().then ( response =>{
 
-        for (let ii = 0; ii < response.length; ii++) {
+            for (let ii = 0; ii < response.length; ii++) {
 
-            if (response[ii].get('SHORTCODE') === items) {
-                hasShortcode = true;
-                const fromRows = Object.values(response[ii].toObject());
+                if (response[ii].get('SHORTCODE') === items) {
+                    hasShortcode = true;
+                    const fromRows = Object.values(response[ii].toObject());
 
-                for (let iii = 0; iii < fromRows.length; iii++) {
-                    if (fromRows[iii] != undefined || fromRows[iii] != null || fromRows[iii] != "") {
-                        if (!newDataUsers.includes(fromRows[iii])) {
-                            newDataUsers.push(fromRows[iii]);
+                    for (let iii = 0; iii < fromRows.length; iii++) {
+                        if (fromRows[iii] != undefined || fromRows[iii] != null || fromRows[iii] != "") {
+                            if (!newDataUsers.includes(fromRows[iii])) {
+                                newDataUsers.push(fromRows[iii]);
+                            }
                         }
                     }
-                }
 
-                return new Promise((resolve) => { 
-                    resolve (newDataUsers);
-                });          
+                        resolve (newDataUsers);
             
+                
+                }
             }
-        }
-        
+            
 
-    });
-
+        });
+    }); 
 }
