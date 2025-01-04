2f82cf569b498088e3374e212e90832098dfa8b0
papiqo <nationalite45@gmail.com>
2025-01-04 17:06:52 +0700

    Update

--- app/reporting/reportInstaLikes.js
+++ app/reporting/reportInstaLikes.js
@@ -128,7 +128,7 @@ export async function reportInstaLikes(clientValue) {
               
             let notLikesName;
 
-              if (notLikesList[iv].get('INSTA') === undefined|| notLikesList[iv].get('INSTA') !== null || notLikesList[iv].get('INSTA') !== ""){
+              if (notLikesList[iv].get('INSTA') === undefined|| notLikesList[iv].get('INSTA') === null || notLikesList[iv].get('INSTA') === ""){
                 notLikesName = "Belum Input";
               } else {
                 notLikesName = notLikesList[iv].get('INSTA');
