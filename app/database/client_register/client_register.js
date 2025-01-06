1bb7304d85a69691e44659b0c3488667787221de
papiqo <nationalite45@gmail.com>
2025-01-06 12:56:13 +0700

    Update

--- app/database/client_register/client_register.js
+++ app/database/client_register/client_register.js
@@ -1,30 +1,45 @@
+import { client } from "../../../app.js";
 import { ciceroKey } from "../new_query/sheet_query.js";
 import { createClientID } from "./create_client_id.js";
 import { createDataSheet } from "./create_data_sheet.js";
+import { createSheetHeader } from "./create_header_rows.js";
 
 export async function clientRegister(clientName, type) {
     
     let headerValues = ['SHORTCODE'];
+    let headerValuesUser = [];
     let headerValuesInsta = [];
     let headerValuesTiktok = [];
     return new Promise(async (resolve, reject) => {
         createClientID(clientName, type).then(
             async data => {
                 console.log(data);
+
+                    //Create User Data
+                    headerValuesUser = ['ID_KEY', 'NAMA', 'TITLE', 'DIVISI', 'JABATAN', 'STATUS', 'WHATSAPP', 'INSTA', 'TIKTOK', 'EXCEPTION'];																];
+
+                    await createDataSheet(clientName, ciceroKey.dbKey.tiktokCommentUsernameID, headerValuesUser, "USER DATA").then(
+                        async data =>{
+                            console.log(data)
+                                }).catch(
+                                    data => reject (data));
+                
                     //Create Insta Data
                     headerValuesInsta = ['TIMESTAMP', 'USER_ACCOUNT', 'SHORTCODE', 'ID', 'TYPE', 'CAPTION',
                         'COMMENT_COUNT', 'LIKE_COUNT', 'PLAY_COUNT'];
 
                     await createDataSheet(clientName, ciceroKey.dbKey.instaOfficialID, headerValuesInsta, "INSTA OFFICIAL").then(
                         async data =>{
-                            console.log(data)
+                            console.log(data);
+                            await client.sendMessage('6281235114745@c.us', data.data);
                             await createDataSheet(clientName, ciceroKey.dbKey.instaLikesUsernameID, headerValues, "INSTA USERNAME DATA").then(
                                 async data =>{
                                      console.log(data);
-                                     console.log(data);
+                                     await client.sendMessage('6281235114745@c.us', data.data);
                                      await createSheetHeader(clientName, ciceroKey.dbKey.instaLikesUsernameID).then (
                                          async data =>{
                                              console.log(data);
+                                             await client.sendMessage('6281235114745@c.us', data.data);
                                          }).catch(
                                              data => reject (data));
                                     }).catch(
@@ -39,13 +54,16 @@ export async function clientRegister(clientName, type) {
                         'LIKE_COUNT', 'PLAY_COUNT', 'SHARE_COUNT', 'REPOST_COUNT'];
                     await createDataSheet(clientName, ciceroKey.dbKey.tiktokOfficialID, headerValuesTiktok, "TIKTOK OFFICIAL").then(
                         async data =>{
-                            console.log(data)
+                            console.log(data);
+                            await client.sendMessage('6281235114745@c.us', data.data);
                             await createDataSheet(clientName, ciceroKey.dbKey.tiktokCommentUsernameID, headerValues, "TIKTOK USERNAME DATA").then(
                                 async data =>{
                                     console.log(data);
+                                    await client.sendMessage('6281235114745@c.us', data.data);
                                     await createSheetHeader(clientName, ciceroKey.dbKey.tiktokCommentUsernameID).then (
                                         async data =>{
                                             console.log(data);
+                                            await client.sendMessage('6281235114745@c.us', data.data);
                                         }).catch(
                                             data => reject (data));
                                 }).catch(
