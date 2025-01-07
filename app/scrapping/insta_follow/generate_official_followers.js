6f15e1c4b386202514fec046f2fbaeb5927a20c8
papiqo <nationalite45@gmail.com>
2025-01-07 13:34:15 +0700

    Update

--- app/scrapping/insta_follow/generate_official_followers.js
+++ app/scrapping/insta_follow/generate_official_followers.js
@@ -1,23 +1,22 @@
 import { instaFollowersAPI } from "../../socialMediaAPI/insta_API.js";
 
-export async function instaUserFollowing(clientName, username, pages, array) {
+export async function instaUserFollowing(clientName, username, pages) {
 
     console.log("Execute insta user following");
     return new Promise(async (resolve, reject) => {
         try {
             await instaFollowersAPI(username, pages).then(
                 async response => {
-                    let childrenArray = array.concat(response.data.data.items);
-                    console.log(childrenArray);
+
                     let pagination = response.data.pagination_token;
 
                     if(response.data.pagination_token != null){
 
-                        await postInstaFollowersOfficial(clientName, response.data).then(
+                        await postInstaFollowersOfficial(clientName, response.data.data.items).then(
                                 async response => {
                                     console.log(response.data);
                                     client.sendMessage(msg.from, response.data);
-                                    instaUserFollowing(clientName, username, pagination, childrenArray);
+                                    await instaUserFollowing(clientName, username, pagination, childrenArray);
                                 });
                     } else {
                         let responseData =  {
