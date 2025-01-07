48f8b92e6aaf08993804e21632f1dc66de329828
papiqo <nationalite45@gmail.com>
2025-01-07 12:54:25 +0700

    Update

--- app/socialMediaAPI/insta_API.js
+++ app/socialMediaAPI/insta_API.js
@@ -4,9 +4,15 @@ import { ciceroKey } from '../database/new_query/sheet_query.js';
 export const { request } = axios;
 
 const headers = {
+  'x-cache-control': 'no-cache',
   'x-rapidapi-key': ciceroKey.instaKey.instakeyAPI,
   'x-rapidapi-host': ciceroKey.instaKey.instahostAPI
 }
+
+const headersnocache = {
+    'x-rapidapi-key': ciceroKey.instaKey.instakeyAPI,
+    'x-rapidapi-host': ciceroKey.instaKey.instahostAPI
+  }
 export async function instaFollowersAPI(key,pagination) {
 
     console.log("Insta Followers API Start");
@@ -19,13 +25,13 @@ export async function instaFollowersAPI(key,pagination) {
             username_or_id_or_url: key,
             pagination_token: pagination
           },
-        headers: headers
+        headers: headersnocache
     };
 
     return new Promise(async(resolve, reject) => {
         try {
             let response = await request(options);
-
+            
             console.log(response.data);
 
             let data = {
