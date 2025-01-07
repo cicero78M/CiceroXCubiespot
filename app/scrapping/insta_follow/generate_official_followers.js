5dcb42bcba143bfde7fcbffdebc93d3f3bbda1e1
papiqo <nationalite45@gmail.com>
2025-01-07 20:33:59 +0700

    Update

--- app/scrapping/insta_follow/generate_official_followers.js
+++ app/scrapping/insta_follow/generate_official_followers.js
@@ -11,17 +11,20 @@ export async function instaUserFollowing(clientName, username, pages, array) {
                 await instaFollowersAPI(username, pages).then(
                     async response => {
 
-                        let pagination = response.data.pagination_token;
-                        let total = response.data.data.total ;
+                        let pagination = await response.data.pagination_token;
+                        let total = await response.data.data.total ;
 
-                        let followersList = array.concat(response.data.data.items);
-                        console.log(total);
+                        let followersList = await array.concat(response.data.data.items);
 
                         console.log(followersList);
 
-                        console.log(followersList.lenght);
+                        console.log(total);
 
-                        if(total > followersList.lenght){
+                        let arrayLenght = Array.isArray(followersList).lenght;
+
+                        console.log(arrayLenght);
+
+                        if(total > arrayLenght){
 
                             console.log("Under Total");
                             setTimeout(async () => {
