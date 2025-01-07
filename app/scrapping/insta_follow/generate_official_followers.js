f4902df07c0ef9846a1cc6ddb7912a7864fdb454
papiqo <nationalite45@gmail.com>
2025-01-07 12:25:49 +0700

    Update

--- app/scrapping/insta_follow/generate_official_followers.js
+++ app/scrapping/insta_follow/generate_official_followers.js
@@ -1,4 +1,4 @@
-import { instaFollowingAPI } from "../socialMediaAPI/insta_API.js";
+import { instaFollowingAPI } from "../../socialMediaAPI/insta_API.js";
 
 export async function instaUserFollowing(username, pages, array) {
     return new Promise(async (resolve, reject) => {
