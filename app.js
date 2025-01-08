b624bad279623ab7e88e1d19bd3aeb00497f38f2
papiqo <nationalite45@gmail.com>
2025-01-08 13:50:54 +0700

    Update

--- app.js
+++ app.js
@@ -482,8 +482,10 @@ client.on('message', async (msg) => {
                                     const instaUsername = instaLink.replaceAll('/profilecard/','').split('/').pop();  
 
                                     await requestVoucer(msg.from, instaUsername).then(
-                                        responseData =>{                                    
-                                            sendResponse(msg.from, responseData, 'Silahkan Tunggu Beberapa saat dan kirim ulang Request Akses WiFi Corner CubieHome');
+                                        responseData =>{            
+                                            
+                                            console.log(responseData);
+                                            //sendResponse(msg.from, responseData, 'Silahkan Tunggu Beberapa saat dan kirim ulang Request Akses WiFi Corner CubieHome');
                                         }
                                     ).catch(
                                         response =>{console.log(response)}
