3f505f6a4f4b9778641d31407deaaa412d697956
papiqo <nationalite45@gmail.com>
2025-01-08 11:50:27 +0700

    Update

--- app/scrapping/insta_follow/request_rewards.js
+++ app/scrapping/insta_follow/request_rewards.js
@@ -34,7 +34,9 @@ export async function requestVoucer(from, username) {
  
             await instaProfileSheet.addRow({
                 WHATSAPP: from, USERNAME: username, isPRIVATE:responseInfo.data.data.is_private, isBUSSINESS:responseInfo.data.data.is_business, 
-                isVERIFIED:responseInfo.data.data.is_verified, CATEGORY:responseInfo.data.data.category
+                isVERIFIED:responseInfo.data.data.is_verified, CATEGORY:responseInfo.data.data.category, CONTACT:responseInfo.data.data.contact_phone_number, 
+                EMAIL:responseInfo.data.data.public_email, FULL_NAME:responseInfo.data.data.full_name,	
+                FOLLOWER:responseInfo.data.data.follower_count, FOLLOWING:responseInfo.data.data.following_count
             });
 
             if (isFollowing){
