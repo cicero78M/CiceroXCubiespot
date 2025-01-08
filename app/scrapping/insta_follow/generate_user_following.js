2bc14e6c49a266e82458645f84330f7c5811fcba
papiqo <nationalite45@gmail.com>
2025-01-08 15:20:12 +0700

    Update

--- app/scrapping/insta_follow/generate_user_following.js
+++ app/scrapping/insta_follow/generate_user_following.js
@@ -16,10 +16,10 @@ export async function instaUserFollowing(username, pages, countData, totalData) 
                 await instaFollowingAPI(username, pages).then(
                     async response => {
 
-                        let dataFollowing = response.data.items;
+                        let dataFollowing = response.data.data.items;
                         let pagination = response.data.pagination_token;
                         let count = response.data.data.count ;
-                        
+
                         console.log(dataFollowing);
 
                         for (let i = 0; i < dataFollowing; i++ ){
