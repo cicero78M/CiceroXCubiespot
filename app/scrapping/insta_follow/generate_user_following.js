fce7b07c068ff72e1e97cef18e12560ed4c07eda
papiqo <nationalite45@gmail.com>
2025-01-08 15:09:55 +0700

    Update

--- app/scrapping/insta_follow/generate_user_following.js
+++ app/scrapping/insta_follow/generate_user_following.js
@@ -1,20 +1,18 @@
 import { instaFollowingAPI } from "../../socialMediaAPI/insta_API.js";
 
 export async function instaUserFollowing(username, pages, countData, totalData) {
+    let stateFoll = false;
 
     console.log("Execute insta user following");
 
     return new Promise(async(resolve, reject) => {
-
-        let stateFoll = false;
-
 
         forLoopGenerateFollowing(username, pages, countData, totalData);
 
 
         async function forLoopGenerateFollowing(username, pages, countData, totalData) {
             try {
-
+                console.log(totalData);
                 await instaFollowingAPI(username, pages).then(
                     async response => {
 
@@ -33,17 +31,15 @@ export async function instaUserFollowing(username, pages, countData, totalData) 
     
                         let totalValue = countData + count;    
                         if (stateFoll === false){
-                            
+
                             console.log('execute');
+                            
                             if(totalData > totalValue){
                                 console.log("Under Total");
-    
                                 setTimeout(async () => {
                                     forLoopGenerateFollowing(username, pagination, totalValue, totalData);
                                 }, 2000);
-    
                             } else {
-    
                                 console.log("done");
                                 let responseData =  {
                                     data: false,
@@ -51,7 +47,6 @@ export async function instaUserFollowing(username, pages, countData, totalData) 
                                     state: true
                                 }                
                                 resolve (responseData);   
-                        
                             }   
                         } else {
                             
