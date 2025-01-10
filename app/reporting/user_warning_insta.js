05641d24e7a6dbea51a05697221b23b879f2f310
papiqo <nationalite45@gmail.com>
2025-01-10 12:33:42 +0700

    Update

--- app/reporting/user_warning_insta.js
+++ app/reporting/user_warning_insta.js
@@ -125,11 +125,14 @@ export async function warningReportInsta(clientValue) {
                             console.log(`Send Warning messages to `);  
                             client.sendMessage(
                                 `${notLikesList[i].get('WHATSAAP')}@c.us`,
-                                `Selamat Siang, Sistem kami membaca bahwa Anda belum melaksanakan Likes dan Komentar pada Konten dari AKun Official  berikut :
+                                `Selamat Siang, Bpk/Ibu ${notLikesList[i].get('TITLE')} ${notLikesList[i].get('NAMA')} 
+                                Sistem kami membaca bahwa Anda belum melaksanakan Likes dan Komentar pada Konten dari AKun Official  berikut :
                                 
                                 ${shortcodeListString}
 
-                                Silahkan segera melaksanakan Likes dan Komentar PAda Kesempatan Pertama, Terimakasih.
+                                Silahkan segera melaksanakan Likes dan Komentar Pada Kesempatan Pertama, Terimakasih.
+
+                                _Anda Menerima Pesan Otomatis ini karena nomor ini terdaftar sesuai dengan Nama User Tercantum, silahkan Save No WA Bot Pegiat Medsos ini_
 
                                 _Cicero System_
                                 `
