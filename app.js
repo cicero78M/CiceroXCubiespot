23d941e897fd8c1a6b94331b803166a206557d86
papiqo <nationalite45@gmail.com>
2025-01-10 16:11:25 +0700

    Update

--- app.js
+++ app.js
@@ -131,19 +131,19 @@ client.on('ready', () => {
     });
 
     // Reload Tiktok every hours until 22
-    schedule('35 16-20 * * *',  () => {
+    schedule('30 16-20 * * *',  () => {
         console.log("Execute Schedule");
 
         schedullerAllSocmed("routine"); //Scheduler Function, routine catch generated data every hours
     });
 
-    schedule('5 15,18,20 * * *',  () => {
+    schedule('0 15,18,20 * * *',  () => {
         console.log("Execute Schedule");
 
         schedullerAllSocmed("report"); //Scheduller Function, report catch and send generated data to Administrator and Operator
     });
 
-    schedule('12 12,16,19 * * *',  () => {
+    schedule('0 12,16,19 * * *',  () => {
 
         console.log("Execute Schedule");
         newRowsData(
