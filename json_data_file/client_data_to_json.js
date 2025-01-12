a0e3cb8c52d7ae7f7b3e8828544629d48e1a6086
papiqo <nationalite45@gmail.com>
2025-01-12 19:28:51 +0700

    Update

--- json_data_file/client_data_to_json.js
+++ json_data_file/client_data_to_json.js
@@ -1,11 +1,13 @@
 import { ciceroKey, newRowsData } from "../app/database/new_query/sheet_query.js";
 
 export async function clientData2Json() {
-    
+
     await newRowsData(
         ciceroKey.dbKey.clientDataID, 
         'ClientData'
     ).then(
-        response => console.log(response)
+        response => {
+            console.log(JSON.parse(response))
+        }
     );
 }
