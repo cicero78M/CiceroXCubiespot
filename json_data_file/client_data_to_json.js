2542693fa35aa36ce6f2a8a7ffd5be3ac5f7b559
papiqo <nationalite45@gmail.com>
2025-01-13 07:35:12 +0700

    Update

--- json_data_file/client_data_to_json.js
+++ json_data_file/client_data_to_json.js
@@ -1,7 +1,7 @@
-import path from "path";
 import { ciceroKey, newRowsData } from "../app/database/new_query/sheet_query.js";
+import { existsSync } from "fs";
 
-    if(path('json_data_file/client_data.json')){
+    if(existsSync('json_data_file/client_data.json')){
 
         console.log("File Exists");
 
