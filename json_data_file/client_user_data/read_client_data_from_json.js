563c024703744e0324caf05fb152724bc7b61f11
papiqo <nationalite45@gmail.com>
2025-01-13 13:52:58 +0700

    Update

--- json_data_file/client_user_data/read_client_data_from_json.js
+++ json_data_file/client_user_data/read_client_data_from_json.js
@@ -1,6 +1,28 @@
 import { readFileSync } from "fs";
 
-export function clientData() {
+export async function clientData() {
     let data = JSON.parse(readFileSync('json_data_file/client_user_data/client_data.json'));
-    console.log(data);
+
+    for (let i = 0; i < data.lenght; i++){
+        let dataItems = data[i];
+
+        let clientData = new Object();
+                        
+        clientData.CLIENT_ID = await decrypted(dataItems[i].get("CLIENT_ID"));
+        clientData.TYPE = await decrypted(dataItems[i].get("TYPE"));
+        clientData.STATUS = await decrypted(dataItems[i].get("STATUS"));
+        clientData.INSTAGRAM = await decrypted(dataItems[i].get("INSTAGRAM"));
+        clientData.TIKTOK = await decrypted(dataItems[i].get("TIKTOK"));
+        clientData.INSTA_STATE = await decrypted(dataItems[i].get("INSTA_STATE"));
+        clientData.TIKTOK_STATE = await decrypted(dataItems[i].get("TIKTOK_STATE"));
+        clientData.SUPERVISOR = await decrypted(dataItems[i].get("SUPERVISOR"));
+        clientData.OPERATOR = await decrypted(dataItems[i].get("OPERATOR"));
+        clientData.GROUP = await decrypted(dataItems[i].get("GROUP"));
+        clientData.SECUID = await decrypted(dataItems[i].get("SECUID"));
+
+        console.log(clientData);
+
+
+    }
+
 }
