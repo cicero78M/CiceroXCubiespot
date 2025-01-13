81a5539ce70b7afebb9522c5386dee042c6d61dd
papiqo <nationalite45@gmail.com>
2025-01-13 22:50:34 +0700

    Update

--- json_data_file/user_data/transfer_user_data_to_json.js
+++ json_data_file/user_data/transfer_user_data_to_json.js
@@ -5,39 +5,36 @@ import { writeFileSync } from 'fs'
 export async function transferUserData(clientID) {
     return new Promise(async (resolve, reject) => {
 
-    try {
-        await newRowsData(
-            ciceroKey.dbKey.userDataID, 
-            clientID
-        ).then(
-            async data =>{
-                let client = [];
-                
-                for (let i = 0; i < data.length; i++){
-    
-                    let userData = new Object();
-                    userData.ID_KEY = await encrypted(data[i].get("ID_KEY"));
-                    userData.NAMA = await encrypted(data[i].get("NAMA"));
-                    userData.TITLE = await encrypted(data[i].get("TITLE"));
-                    userData.DIVISI = await encrypted(data[i].get("DIVISI"));
-                    userData.JABATAN = await encrypted(data[i].get("JABATAN"));
-                    userData.STATUS = await encrypted(data[i].get("STATUS"));
-                    userData.WHATSAPP = await encrypted(data[i].get("WHATSAPP"));
-                    userData.INSTA = await encrypted(data[i].get("INSTA"));
-                    userData.TIKTOK = await encrypted(data[i].get("TIKTOK"));
-                    userData.EXCEPTION = await encrypted(data[i].get("EXCEPTION"));
-                    client.push(userData);
-                };
-    
-                writeFileSync(`json_data_file/user_data/${clientID}.json`, JSON.stringify(client));
-                resolve (`${clientID} JSON Data Successfully Added.`);
-            }
-        );
-    } catch (error) {
-        reject (error)
-    }
-
+        try {
+            await newRowsData(
+                ciceroKey.dbKey.userDataID, 
+                clientID
+            ).then(
+                async data =>{
+                    let client = [];
+                    
+                    for (let i = 0; i < data.length; i++){
         
+                        let userData = new Object();
+                        userData.ID_KEY = await encrypted(data[i].get("ID_KEY"));
+                        userData.NAMA = await encrypted(data[i].get("NAMA"));
+                        userData.TITLE = await encrypted(data[i].get("TITLE"));
+                        userData.DIVISI = await encrypted(data[i].get("DIVISI"));
+                        userData.JABATAN = await encrypted(data[i].get("JABATAN"));
+                        userData.STATUS = await encrypted(data[i].get("STATUS"));
+                        userData.WHATSAPP = await encrypted(data[i].get("WHATSAPP"));
+                        userData.INSTA = await encrypted(data[i].get("INSTA"));
+                        userData.TIKTOK = await encrypted(data[i].get("TIKTOK"));
+                        userData.EXCEPTION = await encrypted(data[i].get("EXCEPTION"));
+                        client.push(userData);
+                    };
+        
+                    writeFileSync(`json_data_file/user_data/${clientID}.json`, JSON.stringify(client));
+                    resolve (`${clientID} JSON Data Successfully Added.`);
+                }
+            );
+        } catch (error) {
+            reject (error)
+        }
     });
-
 }
