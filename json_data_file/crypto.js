8be01c8f9a4cdbbae76a2bc5b209760369591a7a
papiqo <nationalite45@gmail.com>
2025-01-13 13:09:11 +0700

    Update

--- json_data_file/crypto.js
+++ json_data_file/crypto.js
@@ -1,5 +1,5 @@
 import CryptoJS from "crypto-js"
 
-export function encrypted(data) {
+export async function encrypted(data) {
     return CryptoJS.AES.decrypt(data, process.env.SECRET_KEY).toString();
 }
