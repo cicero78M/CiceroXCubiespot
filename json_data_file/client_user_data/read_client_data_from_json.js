7890685655f3dcc390f7fc57a2b565ec6bf3d7ec
papiqo <nationalite45@gmail.com>
2025-01-13 16:06:11 +0700

    Update

--- json_data_file/client_user_data/read_client_data_from_json.js
+++ json_data_file/client_user_data/read_client_data_from_json.js
@@ -4,33 +4,33 @@ import { decrypted } from "../crypto.js";
 export async function clientData() {
     let data = JSON.parse(readFileSync('json_data_file/client_user_data/client_data.json'));
     console.log(data);
-    // let client = [];
+    let client = [];
 
-    // for (let i = 0; i < data.lenght; i++){
+    for (let i = 0; i < data.lenght; i++){
 
-    //     console.log(decrypted(dataItems[i].get("INSTAGRAM"));)
+        console.log(decrypted(dataItems[i].get("INSTAGRAM")));
 
-    //     let dataItems = data[i];
+        let dataItems = data[i];
 
-    //     let clientData = new Object();
+        let clientData = new Object();
                         
-    //     clientData.CLIENT_ID = await decrypted(dataItems[i].get("CLIENT_ID"));
-    //     clientData.TYPE = await decrypted(dataItems[i].get("TYPE"));
-    //     clientData.STATUS = await decrypted(dataItems[i].get("STATUS"));
-    //     clientData.INSTAGRAM = await decrypted(dataItems[i].get("INSTAGRAM"));
-    //     clientData.TIKTOK = await decrypted(dataItems[i].get("TIKTOK"));
-    //     clientData.INSTA_STATE = await decrypted(dataItems[i].get("INSTA_STATE"));
-    //     clientData.TIKTOK_STATE = await decrypted(dataItems[i].get("TIKTOK_STATE"));
-    //     clientData.SUPERVISOR = await decrypted(dataItems[i].get("SUPERVISOR"));
-    //     clientData.OPERATOR = await decrypted(dataItems[i].get("OPERATOR"));
-    //     clientData.GROUP = await decrypted(dataItems[i].get("GROUP"));
-    //     clientData.SECUID = await decrypted(dataItems[i].get("SECUID"));
+        clientData.CLIENT_ID = await decrypted(dataItems[i].get("CLIENT_ID"));
+        clientData.TYPE = await decrypted(dataItems[i].get("TYPE"));
+        clientData.STATUS = await decrypted(dataItems[i].get("STATUS"));
+        clientData.INSTAGRAM = await decrypted(dataItems[i].get("INSTAGRAM"));
+        clientData.TIKTOK = await decrypted(dataItems[i].get("TIKTOK"));
+        clientData.INSTA_STATE = await decrypted(dataItems[i].get("INSTA_STATE"));
+        clientData.TIKTOK_STATE = await decrypted(dataItems[i].get("TIKTOK_STATE"));
+        clientData.SUPERVISOR = await decrypted(dataItems[i].get("SUPERVISOR"));
+        clientData.OPERATOR = await decrypted(dataItems[i].get("OPERATOR"));
+        clientData.GROUP = await decrypted(dataItems[i].get("GROUP"));
+        clientData.SECUID = await decrypted(dataItems[i].get("SECUID"));
 
-    //     client.push(clientData);
+        client.push(clientData);
 
 
-    //     console.log(client);
+        console.log(client);
 
-    //}
+    }
 
 }
