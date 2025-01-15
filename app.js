ff78cda2af8613474dff9f938c5da99f95a94221
papiqo <nationalite45@gmail.com>
2025-01-15 10:28:33 +0700

    Update

--- app.js
+++ app.js
@@ -321,7 +321,7 @@ client.on('message', async (msg) => {
                                 let sheetID = url.pathname.slice(1);
 
                                 if (sheetID.includes('edit')){
-                                    console.log(sheetID.split('/')[-2])
+                                    console.log(sheetID.split('/')[0])
                                 } else {
                                     console.log(sheetID.split('/').pop());
                                 }
