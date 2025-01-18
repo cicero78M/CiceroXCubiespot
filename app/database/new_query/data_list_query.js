45d7f4fadd6fd37a10051cb4845f98ca61a2bd24
papiqo <nationalite45@gmail.com>
2025-01-18 21:46:24 +0700

    Update

--- app/database/new_query/data_list_query.js
+++ app/database/new_query/data_list_query.js
@@ -13,7 +13,6 @@ export async function newListValueData(
         
             await readUser(clientName).then(
             data =>{
-                    userRows = data;
                     
                     for (let i = 0; i < data.length; i++) {            
                         
