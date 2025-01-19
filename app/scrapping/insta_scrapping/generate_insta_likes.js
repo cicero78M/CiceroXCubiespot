a16e13a2c7f14e33bcd30edeeb954c1d8b98e158
papiqo <nationalite45@gmail.com>
2025-01-19 22:41:23 +0700

    Update

--- app/scrapping/insta_scrapping/generate_insta_likes.js
+++ app/scrapping/insta_scrapping/generate_insta_likes.js
@@ -1,6 +1,8 @@
 import { instaLikesAPI } from '../../socialMediaAPI/insta_API.js';
 import { client } from '../../../app.js';
 import { decrypted, encrypted } from '../../../json_data_file/crypto.js';
+import { mkdirSync, readdirSync, writeFileSync } from "fs";
+
 
 export async function getInstaLikes(todayItems, clientValue ) {
 
