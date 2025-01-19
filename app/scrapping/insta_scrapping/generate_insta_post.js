9fab0c4a6f6de8132d92c4ff3a02e44c6998e7d5
papiqo <nationalite45@gmail.com>
2025-01-19 13:48:50 +0700

    Update

--- app/scrapping/insta_scrapping/generate_insta_post.js
+++ app/scrapping/insta_scrapping/generate_insta_post.js
@@ -93,7 +93,7 @@ export async function getInstaPost(clientValue) {
                 for (let ii = 0; ii < officialInstaData.length; ii++) {
                   if (officialInstaData[ii].get('SHORTCODE') === itemByDay[i].code) {
 
-                    writeFileSync(`json_data_file/insta_data/insta_content/${clientName}/${itemByDay[i].code}.json`, JSON.stringify(itemByDay[i]));
+                    // writeFileSync(`json_data_file/insta_data/insta_content/${clientName}/${itemByDay[i].code}.json`, JSON.stringify(itemByDay[i]));
 
                     //Update Existing Content Database                
                     officialInstaData[ii].assign({
@@ -105,7 +105,7 @@ export async function getInstaPost(clientValue) {
                     shortcodeUpdateCounter++;
                   } else if (!shortcodeList.includes(itemByDay[i].code)) {
                     //Push New Content to Database
-                    writeFileSync(`json_data_file/insta_data/insta_content/${clientName}/${itemByDay[i].code}.json`, JSON.stringify(itemByDay[i]));
+                    // writeFileSync(`json_data_file/insta_data/insta_content/${clientName}/${itemByDay[i].code}.json`, JSON.stringify(itemByDay[i]));
   
                     shortcodeList.push(itemByDay[i].code);
                     await officialInstaSheet.addRow({
@@ -128,17 +128,17 @@ export async function getInstaPost(clientValue) {
 
                 shortcodeNewCounter++;
               
-                try {
+                // try {
 
-                  writeFileSync(`json_data_file/insta_data/insta_content/${clientName}/${itemByDay[i].code}.json`, JSON.stringify(itemByDay[i]));
+                //   writeFileSync(`json_data_file/insta_data/insta_content/${clientName}/${itemByDay[i].code}.json`, JSON.stringify(itemByDay[i]));
         
-                } catch (error) {
+                // } catch (error) {
       
-                  mkdirSync(`json_data_file/insta_data/insta_content/${clientName}`);
-                  writeFileSync(`json_data_file/insta_data/insta_content/${clientName}/${itemByDay[i].code}.json`, JSON.stringify(itemByDay[i]));
+                //   mkdirSync(`json_data_file/insta_data/insta_content/${clientName}`);
+                //   writeFileSync(`json_data_file/insta_data/insta_content/${clientName}/${itemByDay[i].code}.json`, JSON.stringify(itemByDay[i]));
                     
-                }
-                resolve (`${itemByDay[i].code} JSON Data Successfully Added.`);
+                // }
+                // resolve (`${itemByDay[i].code} JSON Data Successfully Added.`);
               }
             }
             let data = {
