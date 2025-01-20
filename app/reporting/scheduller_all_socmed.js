29e348b1c6d3c5b446df9a5072306cc41993dfd0
papiqo <nationalite45@gmail.com>
2025-01-20 19:20:40 +0700

    Update

--- app/reporting/scheduller_all_socmed.js
+++ app/reporting/scheduller_all_socmed.js
@@ -24,6 +24,7 @@ export async function schedullerAllSocmed(timeSwitch) {
 
         await clientData().then( 
             async clientData =>{
+                console.log(clientData);
 
                 for (let i = 0; i < clientData.length; i++){
             
