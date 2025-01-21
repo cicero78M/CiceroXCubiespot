2bb9811ccb0e27ffc8e2af4a42a130935bfcf200
papiqo <nationalite45@gmail.com>
2025-01-21 12:37:01 +0700

    Update

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ client.on('ready', () => {
         schedullerAllSocmed("report"); //Scheduller Function, report catch and send generated data to Administrator and Operator
     });
 
-    schedule('27 12,16,19 * * *',  () => {
+    schedule('38 12,16,19 * * *',  () => {
         console.log("Execute Schedule");
         clientData().then( async clientData =>{
 
--- app/reporting/user_warning_insta.js
+++ app/reporting/user_warning_insta.js
@@ -45,7 +45,150 @@ export async function warningReportInsta(clientValue) {
 
         // If Client_ID exist. then get Insta content
         
-        if (decrypted(clientValue.STATUS)) {   
+        if (decrypted(clientValue.STATUS)) { 
+
+                  await readUser(
+                    clientName
+                  ).then( 
+                    async response => {    
+                      userRows = await response;                           
+                      for (let i = 0; i < userRows.length; i++) {
+                        if (userRows[i].STATUS === 'TRUE' ){
+                          userAll++;
+                        }
+                      } 
+                    }
+                  );
+          
+                  // If Client_ID exist. then get Insta content
+          
+                  if (decrypted(clientValue.STATUS)) {   
+          
+                    let instaContentDir = readdirSync(`json_data_file/insta_data/insta_content/${clientName}`);
+          
+                    for (let i = 0; i < instaContentDir.length; i++) {
+          
+                      let contentItems = JSON.parse(readFileSync(`json_data_file/insta_data/insta_content/${clientName}/${instaContentDir[i]}`));
+                      // console.log(contentItems);
+          
+                      let itemDate = new Date(Number(decrypted(contentItems.TIMESTAMP)) * 1000);
+                      let dateNow = itemDate.toLocaleDateString("en-US", {timeZone: "Asia/Jakarta"});
+          
+                      // console.log(itemDate.toLocaleDateString("en-US", {timeZone: "Asia/Jakarta"}));
+                      // console.log(localDate);
+          
+          
+                      if ( dateNow === localDate) {
+          
+                        if (!shortcodeList.includes(decrypted(contentItems.SHORTCODE))) {
+          
+                          shortcodeList.push(decrypted(contentItems.SHORTCODE));
+                          
+                          if (decrypted(contentItems.TYPE) === 'reel') {
+                            shortcodeListString = shortcodeListString.concat('\nhttps://instagram.com/reel/' + decrypted(contentItems.SHORTCODE));
+                          } else {
+                            shortcodeListString = shortcodeListString.concat('\nhttps://instagram.com/p/' + decrypted(contentItems.SHORTCODE));
+                          }
+                        }
+                      }
+                    }
+          
+                    if (shortcodeList.length >= 1) {              
+                      //Collect Likes Data
+                      for (let i = 0; i < shortcodeList.length; i++) {
+          
+                        let likesItems = JSON.parse(readFileSync(`json_data_file/insta_data/insta_likes/${clientName}/${shortcodeList[i]}.json`));
+          
+                        for (let ii = 0; ii < likesItems.length; ii++) {
+          
+                          if (!userLikesData.includes(decrypted(likesItems[ii]))) {
+                            userLikesData.push(decrypted(likesItems[ii]));
+                          }
+                        }
+          
+                      } 
+          
+                      for (let i = 0; i < userRows.length; i++) {     
+          
+                        if (userRows[i].INSTA === undefined
+                        || userRows[i].INSTA === null 
+                        || userRows[i].INSTA === ""){
+          
+                          console.log("Null Data Exist");
+                          UserNotLikes.push(userRows[i].ID_KEY);
+                          notLikesList.push(userRows[i]);
+          
+                        } else {
+                          if (!userLikesData.includes(userRows[i].INSTA)) {
+                            if (!UserNotLikes.includes(userRows[i].ID_KEY)) {
+                              if (userRows[i].STATUS === 'TRUE' ){
+                                if (userRows[i].EXCEPTION === "FALSE"){
+                                  
+                                  UserNotLikes.push(userRows[i].ID_KEY);
+                                  notLikesList.push(userRows[i]);
+                                }                
+                              }
+                            }
+                          } 
+                        }
+               
+                      }
+
+                      for (let i = 0; i < notLikesList.length; i++){
+                        if(notLikesList[i].WHATSAPP !== ""){
+                          
+                          console.log(`Send Warning messages to ${notLikesList[i].TITLE} ${notLikesList[i].NAMA}`);  
+                          await client.sendMessage(
+                              `${notLikesList[i].WHATSAPP}@c.us`,
+                              `Selamat Siang, Bpk/Ibu ${notLikesList[i].TITLE} ${notLikesList[i].NAMA}\n\nSistem kami membaca bahwa Anda belum melaksanakan Likes dan Komentar pada Konten dari AKun Official  berikut :\n\n${shortcodeListString}\n\nSilahkan segera melaksanakan Likes dan Komentar Pada Kesempatan Pertama, Terimakasih.\n\n_Anda Menerima Pesan Otomatis ini karena nomor ini terdaftar sesuai dengan Nama User Tercantum, silahkan Save No WA Bot Pegiat Medsos ini_\n\n_Cicero System_
+                              `
+                          );
+                          setTimeout(async () => {
+                            console.log("Waits");
+                          }, 4000);
+                        }
+                      }
+        
+                      data = {
+                          data: "Send warning Done",
+                          state: true,
+                          code: 200
+                        };
+                        resolve (data);
+                      
+            
+                    } else {
+                      data = {
+                        data: "Tidak ada konten data untuk di olah",
+                        state: true,
+                        code: 201
+                      };
+                      reject (data);
+                    }
+          
+                  } else {
+                    data = {
+                      data: 'Your Client ID has Expired, Contacts Developers for more Informations',
+                      state: true,
+                      code: 201
+                    };
+                    reject (data);
+                  }
+          
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
 
           let instaContentDir = readdirSync(`json_data_file/insta_data/insta_content/${clientName}`);
 
@@ -108,28 +251,7 @@ export async function warningReportInsta(clientValue) {
     
             }
 
-            for (let i = 0; i < notLikesList.length; i++){
-                if(notLikesList[i].WHATSAPP !== ""){
-                  
-                  console.log(`Send Warning messages to ${notLikesList[i].TITLE} ${notLikesList[i].NAMA}`);  
-                  await client.sendMessage(
-                      `${notLikesList[i].WHATSAPP}@c.us`,
-                      `Selamat Siang, Bpk/Ibu ${notLikesList[i].TITLE} ${notLikesList[i].NAMA}\n\nSistem kami membaca bahwa Anda belum melaksanakan Likes dan Komentar pada Konten dari AKun Official  berikut :\n\n${shortcodeListString}\n\nSilahkan segera melaksanakan Likes dan Komentar Pada Kesempatan Pertama, Terimakasih.\n\n_Anda Menerima Pesan Otomatis ini karena nomor ini terdaftar sesuai dengan Nama User Tercantum, silahkan Save No WA Bot Pegiat Medsos ini_\n\n_Cicero System_
-                      `
-                  );
-                  setTimeout(async () => {
-                    console.log("Waits");
-                  }, 4000);
-                }
-            }
 
-            data = {
-                data: "Send warning Done",
-                state: true,
-                code: 200
-              };
-              resolve (data);
-            
           } else {
             data = {
               data: "Tidak ada konten data untuk di olah",
