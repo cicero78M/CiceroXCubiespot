5f3d6702319cd5a9cb79834b5fd6f5752ad61a2b
papiqo <nationalite45@gmail.com>
2025-01-22 21:54:14 +0700

    uPDATE

--- app/backup/insta_likes.js
+++ app/backup/insta_likes.js
@@ -30,8 +30,9 @@ export async function instaLikesBackup(clientValue) {
 
                 let itemDate = new Date(Number(decrypted(contentItems.TIMESTAMP)) * 1000);
                 let dateNow = itemDate.toLocaleDateString("en-US", {timeZone: "Asia/Jakarta"});
-                shortcodeList.push(decrypted(contentItems.SHORTCODE));
                 if ( dateNow === localDate) {
+                  shortcodeList.push(decrypted(contentItems.SHORTCODE));
+
                 }
             }
 
