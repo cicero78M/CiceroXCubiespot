ecf31998b0edfd708b31dc8e1180c7baaf1e5224
papiqo <nationalite45@gmail.com>
2025-01-22 12:21:58 +0700

    Update

--- json_data_file/client_data/read_client_data_from_json.js
+++ json_data_file/client_data/read_client_data_from_json.js
@@ -3,9 +3,11 @@ import { readFileSync  } from "fs";
 export async function clientData() {    
     return new Promise(async (resolve, reject) => {
         try {
+            
+            console.log("Collecting Client Data");
+
             let data = [];
             data = JSON.parse(readFileSync('json_data_file/client_data/client_data.json'));
-            console.log(data.length);
 
             resolve (data);
             
