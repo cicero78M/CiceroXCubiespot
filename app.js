d12156f90bfbda2e547f62d0e54fd61d70ac80fe
papiqo <nationalite45@gmail.com>
2025-01-24 13:58:00 +0700

    Update

--- app.js
+++ app.js
@@ -1478,7 +1478,7 @@ client.on('message', async (msg) => {
                             break;
                     }
                 } else if( detikCom.includes(splittedMsg[1].toLowerCase())){
-                    detikScrapping(includes(splittedMsg[2]));
+                    detikScrapping(splittedMsg[2]);
                 } else {//Key Order Data Not Exist         
 
                     await clientData().then(
