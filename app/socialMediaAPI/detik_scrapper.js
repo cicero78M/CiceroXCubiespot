0aa07ba3f9507320f1a2d83c43bfa54e14c5ecfb
papiqo <nationalite45@gmail.com>
2025-01-24 14:02:51 +0700

    Update

--- app/socialMediaAPI/detik_scrapper.js
+++ app/socialMediaAPI/detik_scrapper.js
@@ -1,33 +1,31 @@
 import puppeteer from "puppeteer";
 
-        
-const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-    executablePath: '/usr/bin/chromium-browser'
 
-});
+    export async function detikScrapping(url) {
 
-const page = await browser.newPage();
+            
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        executablePath: '/usr/bin/chromium-browser'
 
+    });
 
-export async function detikScrapping(url) {
-
-
+    const page = await browser.newPage();
 
     await page.goto(url, {
         waitUntil: "domcontentloaded",
     });
 
-        // Get page data
+      // Get page data
     const document = await page.evaluate(() => {
     // Fetch the first element with class "quote"
-        const comment = document.querySelector(".komentar-iframe-min-comment-body");
+    const comment = document.querySelector(".komentar-iframe-min-comment-body");
 
-        // Fetch the sub-elements from the previously fetched quote element
-        // Get the displayed text and return it (`.innerText`)
-        const commentCount = comment.querySelector(".komentar-iframe-min-font-bold").innerText;
+    // Fetch the sub-elements from the previously fetched quote element
+    // Get the displayed text and return it (`.innerText`)
+    const commentCount = comment.querySelector(".komentar-iframe-min-font-bold").innerText;
 
-        console.log(commentCount)  
-    });
+    console.log(commentCount)  
+});
 }
