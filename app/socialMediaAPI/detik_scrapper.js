97dd29e7fc549c7a5215b5da5f6b6ed7e46b2784
papiqo <nationalite45@gmail.com>
2025-01-24 21:28:49 +0700

    Update

--- app/socialMediaAPI/detik_scrapper.js
+++ app/socialMediaAPI/detik_scrapper.js
@@ -1,42 +1,27 @@
-import puppeteer from "puppeteer";
+import cheerio from 'cheerio';
+import axios from 'axios';
 
 export async function detikScrapping() {   
     try {
-        
-    const browser = await puppeteer.launch({
-        defaultViewport: null,
-        executablePath: '/usr/bin/chromium-browser',
-        ignoreDefaultArgs: ['--disable-extensions']
-    });
-    
-    const page = await browser.newPage();
 
-    await page.goto("https://news.detik.com/berita/d-7745563/hoegeng-awards-2025-resmi-dibuka-saatnya-usulkan-polisi-teladan-di-sekitarmu", {
-        waitUntil: "domcontentloaded",
-      });
-
-      await page.waitForSelector('.komentar-iframe-min-comment-entry');
-      const comments = await page.$$('komentar-iframe-min-comment-entry');
-
-      console.log(comments);
-
-      for (const comment of comments ){
-
-        try {
-
-          const user = await page.evaluate(el => el.querySelector('.komentar-iframe-min-media__user').textContent, comment);
-
-          console.log(user);
-          
-        } catch (error) {
-
-          console.error(error);
+      const axiosResponse = await axios.request({
+        method: "GET",
+        url: "https://news.detik.com/berita/d-7745563/hoegeng-awards-2025-resmi-dibuka-saatnya-usulkan-polisi-teladan-di-sekitarmu",
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
         }
-      }
-       
-    await browser.close();
+      });
+      
+              
+      const $ = cheerio.load(axiosResponse.data);
+
+      $(".komentar-iframe-min-list-content .komentar-iframe-min-list-content--bordered")
+      .find(".komentar-iframe-min-media__user")
+      .each((index, element) => {
+      console.log(index, element);
         
+      });        
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
 }
