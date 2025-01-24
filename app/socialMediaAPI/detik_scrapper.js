5e8756c7d96717ad5c5066f01a6cc6c118760a2a
papiqo <nationalite45@gmail.com>
2025-01-24 14:28:04 +0700

    Update

--- app/socialMediaAPI/detik_scrapper.js
+++ app/socialMediaAPI/detik_scrapper.js
@@ -1,22 +1,24 @@
 import puppeteer from "puppeteer";
 
-const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-    executablePath: '/usr/bin/google-chrome',
-    ignoreDefaultArgs: ['--disable-extensions']
 
-});
 
-const page = await browser.newPage();
+export async function detikScrapping(url) {   
+    const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+        executablePath: '/usr/bin/google-chrome',
+        ignoreDefaultArgs: ['--disable-extensions']
+    });
+    
+    const page = await browser.newPage();
 
-export async function detikScrapping(url) {
-            
     await page.goto(url, {
         waitUntil: "domcontentloaded",
     });
-    const document = await page.waitForSelector(".komentar-iframe-min-comment-body komentar-iframe-min-comment-body--detiknews");
 
-    console.log(document)  
+    console.log(page);
+
+    await browser.close();
+
 
 }
