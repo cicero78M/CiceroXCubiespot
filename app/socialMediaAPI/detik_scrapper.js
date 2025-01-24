a3abe7450f7f9f3d3ff9bfac34aa754d2a82d2b3
papiqo <nationalite45@gmail.com>
2025-01-24 18:36:29 +0700

    Update

--- app/socialMediaAPI/detik_scrapper.js
+++ app/socialMediaAPI/detik_scrapper.js
@@ -2,7 +2,6 @@ import puppeteer from "puppeteer";
 
 export async function detikScrapping() {   
     try {
-
         
     const browser = await puppeteer.launch({
         headless: false,
@@ -16,16 +15,17 @@ export async function detikScrapping() {
     await page.goto("https://news.detik.com/berita/d-7745563/hoegeng-awards-2025-resmi-dibuka-saatnya-usulkan-polisi-teladan-di-sekitarmu", {
         waitUntil: "domcontentloaded",
       });
+      const getHeader = await page.$('h1')
+      const header = await page.evaluate( el => el.innerText, getHeader); 
+
       const link = await page.$eval('a', anchor => anchor.getAttribute('href')); 
 
 
-      console.log (link);
-    
-    
+      console.log (header, link);
+       
     await browser.close();
         
     } catch (error) {
         console.log(error);
     }
-
 }
