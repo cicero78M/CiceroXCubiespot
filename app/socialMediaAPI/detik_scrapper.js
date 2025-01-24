3c99a9094c47254799ea0355ecb73e339990ba83
papiqo <nationalite45@gmail.com>
2025-01-24 16:04:44 +0700

    Update

--- app/socialMediaAPI/detik_scrapper.js
+++ app/socialMediaAPI/detik_scrapper.js
@@ -19,7 +19,7 @@ export async function detikScrapping() {
 
       const quotes = await page.evaluate(() => {
 
-        const quote = document.querySelector("#thecomment2");
+        const quote = document.querySelector(".komentar-iframe-min-media__desc").innerHTML;
 
         return { quote };
       });
