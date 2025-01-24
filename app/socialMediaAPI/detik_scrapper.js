8b4a458191153c4582300210aa90284beec50460
papiqo <nationalite45@gmail.com>
2025-01-24 22:05:49 +0700

    Update

--- app/socialMediaAPI/detik_scrapper.js
+++ app/socialMediaAPI/detik_scrapper.js
@@ -16,7 +16,7 @@ export async function detikScrapping() {
               
       const $ = cheerio.load(axiosResponse.data);
 
-      console.log($('.komentar-iframe-min-comment-entry').html());
+      console.log($('.komentar-iframe-min-media__user').text());
 
     } catch (error) {
       console.log(error);
