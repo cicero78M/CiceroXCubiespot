741285c8620c6f884e10c97fb845510d249ff498
papiqo <nationalite45@gmail.com>
2025-01-25 14:49:14 +0700

    Update

--- app/view/info_view.js
+++ app/view/info_view.js
@@ -4,6 +4,7 @@ export async function infoView(dataResponse) {
     data : 
     `*Berikut Format Pesan WABOT PEGIAT MEDSOS*
     =================================
+    =================================
     *Menampilkan Pesan ini* :
 
     ${dataResponse}#INFO
@@ -38,24 +39,24 @@ export async function infoView(dataResponse) {
     =================================
     *Format Wa Untuk merubah Data Pangkat :*
     
-    ${dataResponse}#EDITPANGKAT#NRP#PANGKAT
+    ${dataResponse}#PANGKAT#NRP#PANGKAT
     
     _Contoh :_ 
-    ${dataResponse}#EDITPANGKAT#87020990#BRIPKA
+    ${dataResponse}#PANGKAT#87020990#BRIPKA
     =================================
     *Format Pesan WA untuk merubah Data Nama Satfung :*
     
-    ${dataResponse}#EDITSATFUNG#NRP#SATFUNG
+    ${dataResponse}#SATFUNG#NRP#SATFUNG
     
     _Contoh :_
-    ${dataResponse}#EDITSATFUNG#87020990#TIK
+    ${dataResponse}#SATFUNG#87020990#TIK
     =================================
     *Format Pesan WA untuk merubah data Jabatan :*
 
-    ${dataResponse}#EDITJABATAN#NRP#JABATAN
+    ${dataResponse}#JABATAN#NRP#JABATAN
     
     _Contoh :_ 
-    ${dataResponse}#EDITJABATAN#87020990#BAMIN
+    ${dataResponse}#JABATAN#87020990#BAMIN
     =================================`,
     state: true,
     code: 200
