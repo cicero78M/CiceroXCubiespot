f1ab9c760b0a26ade2695835056ac2cdbc39395c
papiqo <nationalite45@gmail.com>
2025-01-25 14:54:44 +0700

    Update

--- app/responselogs/response_view.js
+++ app/responselogs/response_view.js
@@ -1,9 +1,9 @@
-//Date Time
-let d = new Date();
-let localDate = d.toLocaleDateString("en-US", {timeZone: "Asia/Jakarta"});
-let hours = d.toLocaleTimeString("en-US", {timeZone: "Asia/Jakarta"});     
-let time = localDate+" >> "+hours;
-
 export function logsResponse(params) {
+    //Date Time
+    let d = new Date();
+    let localDate = d.toLocaleDateString("en-US", {timeZone: "Asia/Jakarta"});
+    let hours = d.toLocaleTimeString("en-US", {timeZone: "Asia/Jakarta"});     
+    let time = localDate+" >> "+hours;
+
     console.log(time+ " >> "+process.env.APP_SESSION_NAME+" >> "+params);
 }
