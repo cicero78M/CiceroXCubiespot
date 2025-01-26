779c888de8c5d400295e22915a26c0aefe0128f4
papiqo <nationalite45@gmail.com>
2025-01-26 18:09:35 +0700

    Update

--- app/view/sendWA.js
+++ app/view/sendWA.js
@@ -1,23 +1,18 @@
 import { client } from "../../app.js";
-import { logsResponse } from "../responselogs/logs_modif.js";
 //Response By User
 export async function sendResponse(from, responseData, errormessage) {
     
     switch (responseData.code){
         case 200:
-            logsResponse("SUCCESS GENERATE DATA");
             await client.sendMessage(from, responseData.data);
             break;
         case 201:
-            logsResponse(responseData);
             await client.sendMessage(from, responseData.data);
             break;
         case 303:                                
-            logsResponse(responseData);
             await client.sendMessage(from, errormessage);
             break;
         default:
-            logsResponse(responseData);
             break;
     }   
 }
@@ -26,23 +21,19 @@ export async function sendClientResponse(clientID, supervisor, operator, group, 
 
     switch (responseData.code){
         case 200 :
-            logsResponse(clientID+' SUCCESS '+type+' DATA');
             await client.sendMessage(supervisor, responseData.data);           
             await client.sendMessage(operator, responseData.data);
             await client.sendMessage(group, responseData.data);
             break;
         case 201:
-            logsResponse(responseData);
             await client.sendMessage(supervisor, responseData.data);
             await client.sendMessage(operator, responseData.data);
             await client.sendMessage(group, responseData.data);
             break;
         case 303 :
-            logsResponse(responseData);
             await client.sendMessage('6281235114745@c.us', clientID+' FAIL '+type+' DATA');
             break;           
         default:
-            logsResponse(responseData);
             break;
     }
 }
