8ea6d37062bcd8c9a7a04bf228716afc41e57f36
papiqo <nationalite45@gmail.com>
2025-01-27 19:12:12 +0700

    Update

--- app/view/notif_view.js
+++ app/view/notif_view.js
@@ -10,17 +10,20 @@ export async function notifView(userData, shhortcodeList) {
                 if(userData[i].WHATSAPP !== ""){
             
                     logsUserSend(
-                        `${userData[i].WHATSAPP}@c.us`,
-        
-                        `Selamat Siang, Bpk/Ibu ${userData[i].TITLE} ${userData[i].NAMA}
-        
-                        Sistem kami membaca bahwa Anda belum melaksanakan Likes dan Komentar pada Konten dari Akun Official  berikut :
-        
-                        ${shhortcodeList}
-        
-                        Silahkan segera melaksanakan Likes dan Komentar Pada Kesempatan Pertama, Terimakasih.
-        
-                        _Anda Menerima Pesan Otomatis ini karena nomor ini terdaftar sesuai dengan Nama User Tercantum, silahkan Save No WA Bot Pegiat Medsos ini_\n\n_Cicero System_`
+                        
+`${userData[i].WHATSAPP}@c.us`,
+
+`Selamat Siang, Bpk/Ibu ${userData[i].TITLE} ${userData[i].NAMA}
+
+Sistem kami membaca bahwa Anda belum melaksanakan Likes dan Komentar pada Konten dari Akun Official  berikut :
+
+${shhortcodeList}
+
+Silahkan segera melaksanakan Likes dan Komentar Pada Kesempatan Pertama, Terimakasih.
+
+_Anda Menerima Pesan Otomatis ini karena nomor ini terdaftar sesuai dengan Nama User Tercantum, silahkan Save No WA Bot Pegiat Medsos ini_
+
+_Cicero System_`
                     );
                   
                   setTimeout(async () => {
