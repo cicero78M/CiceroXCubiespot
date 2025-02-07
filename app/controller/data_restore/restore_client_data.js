8bf8febd7f458da279d8a15a81e9000c2ff50591
papiqo <nationalite45@gmail.com>
2025-02-07 15:30:43 +0700

    Update

--- app/controller/data_restore/restore_client_data.js
+++ app/controller/data_restore/restore_client_data.js
@@ -15,7 +15,6 @@ export async function restoreClientData() {
     ).then(async response => {
 
         let data = response.data;
-        let client = [];
 
         for (let i = 0; i < data.length; i++){
 
@@ -25,44 +24,46 @@ export async function restoreClientData() {
 
           console.log(data[i])
 
+          try {
 
-          if(decrypted(data[i].get("TYPE") === process.env.APP_CLIENT_TYPE)){
+            let clientData = new Object();
+          
+            clientData.CLIENT_ID = data[i].get("CLIENT_ID");
+            clientData.TYPE = data[i].get("TYPE");
+            clientData.STATUS = data[i].get("STATUS");
+            clientData.INSTAGRAM = data[i].get("INSTAGRAM");
+            clientData.TIKTOK = data[i].get("TIKTOK");
+            clientData.INSTA_STATE = data[i].get("INSTA_STATE");
+            clientData.TIKTOK_STATE = data[i].get("TIKTOK_STATE");
+            clientData.SUPERVISOR = data[i].get("SUPERVISOR");
+            clientData.OPERATOR = data[i].get("OPERATOR");
+            clientData.GROUP = data[i].get("GROUP");
+            clientData.SECUID = data[i].get("SECUID");
+            clientData.INSTA_2 = data[i].get("INSTA_2");
+            clientData.INSTA_2_STATE = data[i].get("INSTA_2_STATE");
 
             try {
+              writeFileSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}.json`, JSON.stringify(clientData));
 
-              let clientData = new Object();
-            
-              clientData.CLIENT_ID = data[i].get("CLIENT_ID");
-              clientData.TYPE = data[i].get("TYPE");
-              clientData.STATUS = data[i].get("STATUS");
-              clientData.INSTAGRAM = data[i].get("INSTAGRAM");
-              clientData.TIKTOK = data[i].get("TIKTOK");
-              clientData.INSTA_STATE = data[i].get("INSTA_STATE");
-              clientData.TIKTOK_STATE = data[i].get("TIKTOK_STATE");
-              clientData.SUPERVISOR = data[i].get("SUPERVISOR");
-              clientData.OPERATOR = data[i].get("OPERATOR");
-              clientData.GROUP = data[i].get("GROUP");
-              clientData.SECUID = data[i].get("SECUID");
-              // clientData.INSTA_2 = data[i].get("INSTA_2");
-              // clientData.INSTA_2_STATE = data[i].get("INSTA_2_STATE");
-  
-              try {
-                writeFileSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}.json`, JSON.stringify(clientData));
-
-                console.leg("Success")
-              } catch (error) {
-
-                console.leg("Fail")
-
-                mkdirSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}`);
-                writeFileSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}.json`, JSON.stringify(clientData));
-              } 
-              
+              console.leg("Success")
             } catch (error) {
 
-              console.log(error);
-            }
+              console.leg("Fail")
+
+              mkdirSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}`);
+              writeFileSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}.json`, JSON.stringify(clientData));
+            } 
+            
+          } catch (error) {
+
+            console.log(error);
           }
+
+
+          // if(decrypted(data[i].get("TYPE") === process.env.APP_CLIENT_TYPE)){
+
+
+          // }
         };
     });
 }
