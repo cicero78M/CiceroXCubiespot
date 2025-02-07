3c4fab6ec8383e6c20da81a935b917942eeed8cf
papiqo <nationalite45@gmail.com>
2025-02-07 15:20:35 +0700

    Update

--- app/controller/data_restore/restore_client_data.js
+++ app/controller/data_restore/restore_client_data.js
@@ -25,30 +25,37 @@ export async function restoreClientData() {
 
             console.log(data[i])
 
-            let clientData = new Object();
-            
-            clientData.CLIENT_ID = data[i].get("CLIENT_ID");
-            clientData.TYPE = data[i].get("TYPE");
-            clientData.STATUS = data[i].get("STATUS");
-            clientData.INSTAGRAM = data[i].get("INSTAGRAM");
-            clientData.TIKTOK = data[i].get("TIKTOK");
-            clientData.INSTA_STATE = data[i].get("INSTA_STATE");
-            clientData.TIKTOK_STATE = data[i].get("TIKTOK_STATE");
-            clientData.SUPERVISOR = data[i].get("SUPERVISOR");
-            clientData.OPERATOR = data[i].get("OPERATOR");
-            clientData.GROUP = data[i].get("GROUP");
-            clientData.SECUID = data[i].get("SECUID");
-            clientData.INSTA_2 = data[i].get("INSTA_2");
-            clientData.INSTA_2_STATE = data[i].get("INSTA_2_STATE");
-
-            client.push(clientData);
-
             try {
-              writeFileSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}.json`, JSON.stringify(clientData));
+
+              let clientData = new Object();
+            
+              clientData.CLIENT_ID = data[i].get("CLIENT_ID");
+              clientData.TYPE = data[i].get("TYPE");
+              clientData.STATUS = data[i].get("STATUS");
+              clientData.INSTAGRAM = data[i].get("INSTAGRAM");
+              clientData.TIKTOK = data[i].get("TIKTOK");
+              clientData.INSTA_STATE = data[i].get("INSTA_STATE");
+              clientData.TIKTOK_STATE = data[i].get("TIKTOK_STATE");
+              clientData.SUPERVISOR = data[i].get("SUPERVISOR");
+              clientData.OPERATOR = data[i].get("OPERATOR");
+              clientData.GROUP = data[i].get("GROUP");
+              clientData.SECUID = data[i].get("SECUID");
+              // clientData.INSTA_2 = data[i].get("INSTA_2");
+              // clientData.INSTA_2_STATE = data[i].get("INSTA_2_STATE");
+  
+              try {
+                writeFileSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}.json`, JSON.stringify(clientData));
+              } catch (error) {
+                mkdirSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}`);
+                writeFileSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}.json`, JSON.stringify(clientData));
+              } 
+              
             } catch (error) {
-              mkdirSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}`);
-              writeFileSync(`json_data_file/client_data/${decrypted(data[i].get("CLIENT_ID"))}.json`, JSON.stringify(clientData));
-            } 
+
+              console.log(error);
+            }
+
+
 
           }
         };
