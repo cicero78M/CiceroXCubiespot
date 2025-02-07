1255c62176c0a41fbe4394d60c40092c2a4cdc8b
papiqo <nationalite45@gmail.com>
2025-02-07 14:42:07 +0700

    Update

--- app/controller/data_management/client_list.js
+++ app/controller/data_management/client_list.js
@@ -9,29 +9,59 @@ export async function clientDataView(data) {
 
             if (decrypted(data.TYPE) === process.env.APP_CLIENT_TYPE){
 
-                let response =  {
+                if ( decrypted(data.TYPE) === "RES"){
 
-                    data: 
-                    `Client : ${decrypted(data.CLIENT_ID)}
+                    let response =  {
+
+                        data: 
+                        `Client : ${decrypted(data.CLIENT_ID)}
+        
+                        Account Type : ${decrypted(data.TYPE)}
+                        State : ${decrypted(data.STATUS)}
+                        Insta Primary: ${decrypted(data.INSTAGRAM)}
+                        Insta Primary State : ${decrypted(data.INSTA_STATE)}
+                        Insta Secondary : ${decrypted(data.INSTA_2)}
+                        Insta Secondary State : ${decrypted(data.INSTA_2_STATE)}
+                        Tiktok Primary : ${decrypted(data.TIKTOK)}
+                        Tiktok Primary State : ${decrypted(data.TIKTOK_STATE)}
+                        tiktok Secuid : ${decrypted(data.SECUID)}
+                        Supervisor : ${decrypted(data.SUPERVISOR)}
+                        Operator : ${decrypted(data.OPERATOR)}
+                        Group : ${decrypted(data.GROUP)}
+                        `,
+                        state: true,
+                        code: 200
+                    } 
     
-                    Account Type : ${decrypted(data.TYPE)}
-                    State : ${decrypted(data.STATUS)}
-                    Insta Primary: ${decrypted(data.INSTAGRAM)}
-                    Insta Primary State : ${decrypted(data.INSTA_STATE)}
-                    Insta Secondary : ${decrypted(data.INSTA_2)}
-                    Insta Secondary State : ${decrypted(data.INSTA_2_STATE)}
-                    Tiktok Primary : ${decrypted(data.TIKTOK)}
-                    Tiktok Primary State : ${decrypted(data.TIKTOK_STATE)}
-                    tiktok Secuid : ${decrypted(data.SECUID)}
-                    Supervisor : ${decrypted(data.SUPERVISOR)}
-                    Operator : ${decrypted(data.OPERATOR)}
-                    Group : ${decrypted(data.GROUP)}
-                    `,
-                    state: true,
-                    code: 200
-                } 
+                    resolve (response);
 
-                resolve (response);
+
+                } else {
+
+                    let response =  {
+
+                        data: 
+                        `Client : ${decrypted(data.CLIENT_ID)}
+        
+                        Account Type : ${decrypted(data.TYPE)}
+                        State : ${decrypted(data.STATUS)}
+                        Insta Primary: ${decrypted(data.INSTAGRAM)}
+                        Insta Primary State : ${decrypted(data.INSTA_STATE)}
+                        Tiktok Primary : ${decrypted(data.TIKTOK)}
+                        Tiktok Primary State : ${decrypted(data.TIKTOK_STATE)}
+                        tiktok Secuid : ${decrypted(data.SECUID)}
+                        Supervisor : ${decrypted(data.SUPERVISOR)}
+                        Operator : ${decrypted(data.OPERATOR)}
+                        Group : ${decrypted(data.GROUP)}
+                        `,
+                        state: true,
+                        code: 200
+                    } 
+    
+                    resolve (response);
+                }
+
+
 
             }
 
