5fc93dbe7db7daed02b93b45d9285ef00216e21a
papiqo <nationalite45@gmail.com>
2025-02-07 14:39:21 +0700

    Update

--- app/controller/data_management/client_list.js
+++ app/controller/data_management/client_list.js
@@ -6,29 +6,35 @@ export async function clientDataView(data) {
         try {
             
             logsSave("Collecting Client Data");
-            let response =  {
 
-                data: 
-                `Client : ${decrypted(data.CLIENT_ID)}
+            if (decrypted(data.TYPE) === process.env.APP_CLIENT_TYPE){
 
-                Account Type : ${decrypted(data.TYPE)}
-                State : ${decrypted(data.STATUS)}
-                Insta Primary: ${decrypted(data.INSTAGRAM)}
-                Insta Primary State : ${decrypted(data.INSTA_STATE)}
-                Insta Secondary : ${decrypted(data.INSTA_2)}
-                Insta Secondary State : ${decrypted(data.INSTA_2_STATE)}
-                Tiktok Primary : ${decrypted(data.TIKTOK)}
-                Tiktok Primary State : ${decrypted(data.TIKTOK_STATE)}
-                tiktok Secuid : ${decrypted(data.SECUID)}
-                Supervisor : ${decrypted(data.SUPERVISOR)}
-                Operator : ${decrypted(data.OPERATOR)}
-                Group : ${decrypted(data.GROUP)}
-                `,
-                state: true,
-                code: 200
-            } 
+                let response =  {
 
-            resolve (response);
+                    data: 
+                    `Client : ${decrypted(data.CLIENT_ID)}
+    
+                    Account Type : ${decrypted(data.TYPE)}
+                    State : ${decrypted(data.STATUS)}
+                    Insta Primary: ${decrypted(data.INSTAGRAM)}
+                    Insta Primary State : ${decrypted(data.INSTA_STATE)}
+                    Insta Secondary : ${decrypted(data.INSTA_2)}
+                    Insta Secondary State : ${decrypted(data.INSTA_2_STATE)}
+                    Tiktok Primary : ${decrypted(data.TIKTOK)}
+                    Tiktok Primary State : ${decrypted(data.TIKTOK_STATE)}
+                    tiktok Secuid : ${decrypted(data.SECUID)}
+                    Supervisor : ${decrypted(data.SUPERVISOR)}
+                    Operator : ${decrypted(data.OPERATOR)}
+                    Group : ${decrypted(data.GROUP)}
+                    `,
+                    state: true,
+                    code: 200
+                } 
+
+                resolve (response);
+
+            }
+
             
         } catch (error) {
             let response = {
