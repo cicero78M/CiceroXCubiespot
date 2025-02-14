39eedcf5b415aa9295416da0f554e659010b1ec6
papiqo <nationalite45@gmail.com>
2025-02-14 22:22:44 +0700

    UPdate

--- app/module/crypto.js
+++ app/module/crypto.js
@@ -1,11 +1,22 @@
 import CryptoJS from "crypto-js"
 //Encryptions
 export function encrypted(data) {
-    let encrypt = CryptoJS.AES.encrypt(data, process.env.SECRET_KEY).toString();
-    return encrypt;
+
+    try {
+        let encrypt = CryptoJS.AES.encrypt(data, process.env.SECRET_KEY).toString();
+        return encrypt;
+    } catch (error) {
+        console.log(error)
+    }
+
 }
 //Decryptions
 export function decrypted(data) {
-    let decrypt = CryptoJS.AES.decrypt(data, process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8);
-    return decrypt;
+    try {
+        let decrypt = CryptoJS.AES.decrypt(data, process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8);
+        return decrypt;
+    } catch (error) {
+        console.log(error)
+    }
+
 }
