166e60fe53568b01c7505479065a747bce54c2ef
papiqo <nationalite45@gmail.com>
2025-02-15 12:43:46 +0700

    Update

--- app.js
+++ app.js
@@ -75,15 +75,17 @@ const private_key = process.env;
 // Routing Port 
 const port = private_key.EXPRESS_PORT;
 
-// app.get('/', function (req, res) {
+app.get('/', function (req, res) {
+   res.render("login");
+
     
-//     readUser("BOJONEGORO").then(
-//         response =>{
-//             console.log(response.data);
-//             res.render("index", {data: response.data});
-//         }
-//     )
-// });
+    // readUser("BOJONEGORO").then(
+    //     response =>{
+    //         console.log(response.data);
+    //         res.render("index", {data: response.data});
+    //     }
+    // )
+});
 
 app.listen(port, () => {
     logsSave(`Cicero System Start listening on port >>> ${port}`)
