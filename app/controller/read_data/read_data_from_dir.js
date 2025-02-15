1cceb7ff54e41a28066dd5adfbd31e17d134908f
papiqo <nationalite45@gmail.com>
2025-02-15 12:35:06 +0700

    uPDATE

--- app/controller/read_data/read_data_from_dir.js
+++ app/controller/read_data/read_data_from_dir.js
@@ -18,18 +18,23 @@ export async function readUser(clientName) {
                 
                 let userData = new Object();
 
-                userData.ID_KEY = decrypted(fromJson.ID_KEY);
-                userData.NAMA = decrypted(fromJson.NAMA);
-                userData.TITLE = decrypted(fromJson.TITLE);
-                userData.DIVISI = decrypted(fromJson.DIVISI);
-                userData.JABATAN = decrypted(fromJson.JABATAN);
-                userData.STATUS = decrypted(fromJson.STATUS);
-                userData.WHATSAPP = decrypted(fromJson.WHATSAPP);
-                userData.INSTA = decrypted(fromJson.INSTA);
-                userData.TIKTOK = decrypted(fromJson.TIKTOK);
-                userData.EXCEPTION = decrypted(fromJson.EXCEPTION);
+                if(decrypted(fromJson.STATUS) === "TRUE"){
 
-                client.push(userData);
+                    userData.ID_KEY = decrypted(fromJson.ID_KEY);
+                    userData.NAMA = decrypted(fromJson.NAMA);
+                    userData.TITLE = decrypted(fromJson.TITLE);
+                    userData.DIVISI = decrypted(fromJson.DIVISI);
+                    userData.JABATAN = decrypted(fromJson.JABATAN);
+                    userData.STATUS = decrypted(fromJson.STATUS);
+                    userData.WHATSAPP = decrypted(fromJson.WHATSAPP);
+                    userData.INSTA = decrypted(fromJson.INSTA);
+                    userData.TIKTOK = decrypted(fromJson.TIKTOK);
+                    userData.EXCEPTION = decrypted(fromJson.EXCEPTION);
+
+                    client.push(userData);
+
+                }
+
             }    
             let data = {
                 data: client,
